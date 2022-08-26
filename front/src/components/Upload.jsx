ab2dc08b21543ea7ccf35bd572b551b6252d8e8c
maximi4321 <maximiliano.aguilar@ug.uchile.cl>
2022-08-26 18:37:44 -0400

    deleted upload file button

--- front/src/components/Upload.jsx
+++ front/src/components/Upload.jsx
@@ -1,4 +1,5 @@
-import React, { useState } from 'react';
+import React from 'react';
+// import React, { useState } from 'react';
 import Form from 'react-bootstrap/Form';
 import PropTypes from 'prop-types';
 
@@ -6,27 +7,30 @@ function Upload({ setModels }) {
   Upload.propTypes = {
     setModels: PropTypes.func.isRequired,
   };
-  const [file, setFile] = useState(null);
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-
+  // const [file, setFile] = useState(null);
+  // const handleSubmit = async (e) => {
+  //   e.preventDefault();
+  //
+  //   const formData = new FormData();
+  //   formData.append('file', file);
+  //   const fetchedModels = await fetch('http://localhost:8000/dataset/upload/', { method: 'POST', body: formData });
+  //   const models = await fetchedModels.json();
+  //   setModels(models.models);
+  // };
+  const handleFileSelect = async (e) => {
+    // setFile(e.target.files[0]);
     const formData = new FormData();
-    formData.append('file', file);
-    const fetchedModels = await fetch(`http://localhost:8000/dataset/upload/${file.name}`, { method: 'POST', body: formData });
+    formData.append('file', e.target.files[0]);
+    const fetchedModels = await fetch('http://localhost:8000/dataset/upload/', { method: 'POST', body: formData });
     const models = await fetchedModels.json();
     setModels(models.models);
   };
 
-  const handleFileSelect = (e) => {
-    setFile(e.target.files[0]);
-  };
-
   return (
-    <Form onSubmit={handleSubmit}>
+    <Form>
       <Form.Group controlId="formFileLg" className="mb-3">
         <Form.Label>Upload your dataset.</Form.Label>
         <Form.Control type="file" onChange={handleFileSelect} />
-        <input type="submit" value="Upload File" />
       </Form.Group>
     </Form>
   );
