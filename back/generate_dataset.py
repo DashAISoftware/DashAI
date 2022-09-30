170f89093c200500039da43519bd645ac02b005f
Riul1999 <rodrigo.urrea@ing.uchile.cl>
2022-09-29 21:25:10 -0400

    add new twitter dataset

--- back/generate_dataset.py
+++ back/generate_dataset.py
@@ -1,39 +1,73 @@
 import csv
 import json
 
-csvFilePath = "Example_dataset/Twitter_Data.csv"
-jsonFilePath = "Example_dataset/twitterDataset.json"
+csvFilePath1 = "example_dataset/twitter_training.csv"
+csvFilePath2 = "example_dataset/twitter_validation.csv"
+jsonFilePath = "example_dataset/twitterDataset.json"
 
 size = 1000
 
-data = {"train": {"x": [], "y": []}, "test": {"x": [], "y": []}}
+data = {
+    "task_info":{
+        "task_type":"TextClassificationTask"
+    },
+    "train": {"x": [], "y": []}, 
+    "test": {"x": [], "y": []}}
 
-with open(csvFilePath, encoding="utf8") as csvFile:
+i = 0
+with open(csvFilePath1, encoding="utf8") as csvFile:
     csvReader = csv.DictReader(csvFile)
-    c = 0
-    p = 0
+    keys = []
     for row in csvReader:
-        try:
-            actual_data = [row["clean_text"], int(row["category"])]
-        except Exception as e:
-            print(e)
-            continue
+        keys = list(row.keys())
+        data["train"]["x"].append(row[keys[3]])
+        data["train"]["y"].append(row[keys[2]])
 
-        if actual_data[1] == 0:
-            continue
-
-        if c == size:
-            c = 0
-            p += 1
-        if p == 0:
-            data["train"]["x"].append(actual_data[0])
-            data["train"]["y"].append(actual_data[1])
-        elif p == 1:
-            data["test"]["x"].append(actual_data[0])
-            data["test"]["y"].append(actual_data[1])
-        else:
+        i += 1
+        if i > 5000:
             break
-        c += 1
+
+i = 0
+with open(csvFilePath2, encoding="utf8") as csvFile:
+    csvReader = csv.DictReader(csvFile)
+    keys = []
+    for row in csvReader:
+        keys = list(row.keys())
+        data["test"]["x"].append(row[keys[3]])
+        data["test"]["y"].append(row[keys[2]])
+        
+        i += 1
+        if i > 5000:
+            break
 
 with open(jsonFilePath, "w") as jsonFile:
     jsonFile.write(json.dumps(data, indent=4))
+
+# with open(csvFilePath, encoding="utf8") as csvFile:
+#     csvReader = csv.DictReader(csvFile)
+#     c = 0
+#     p = 0
+#     for row in csvReader:
+#         try:
+#             actual_data = [row["clean_text"], int(row["category"])]
+#         except Exception as e:
+#             print(e)
+#             continue
+
+#         if actual_data[1] == 0:
+#             continue
+
+#         if c == size:
+#             c = 0
+#             p += 1
+#         if p == 0:
+#             data["train"]["x"].append(actual_data[0])
+#             data["train"]["y"].append(actual_data[1])
+#         elif p == 1:
+#             data["test"]["x"].append(actual_data[0])
+#             data["test"]["y"].append(actual_data[1])
+#         else:
+#             break
+#         c += 1
+
+
