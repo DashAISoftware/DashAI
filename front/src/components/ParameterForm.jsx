48fd14ec510dc29da052d91b8001bea1447ff1dc
maximi4321 <maximiliano.aguilar@ug.uchile.cl>
2022-09-29 23:47:18 -0300

    feat: recursive parameters

--- front/src/components/ParameterForm.jsx
+++ front/src/components/ParameterForm.jsx
@@ -1,12 +1,69 @@
-import React, { useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import {
   Card,
   Button,
   Accordion,
 } from 'react-bootstrap';
 import PropTypes from 'prop-types';
-import uuid from 'react-uuid';
 import { useFormik } from 'formik';
+
+function ClassInput({ modelName, paramJsonSchema, setFieldValue }) {
+  ClassInput.propTypes = {
+    modelName: PropTypes.string.isRequired,
+    paramJsonSchema: PropTypes.objectOf(
+      PropTypes.oneOfType([
+        PropTypes.string,
+        PropTypes.bool,
+        PropTypes.object,
+      ]),
+    ).isRequired,
+    setFieldValue: PropTypes.func.isRequired,
+  };
+  const [options, setOptions] = useState([]);
+  const [selectedOption, setSelectedOption] = useState('KNN');
+  const [paramSchema, setParamSchema] = useState({});
+  const getOptions = async (parentClass) => {
+    const fetchedOptions = await fetch(
+      `http://localhost:8000/getChildren/${parentClass}`,
+    );
+    const receivedOptions = await fetchedOptions.json();
+    setOptions(receivedOptions);
+    console.log(receivedOptions);
+  };
+  const getParamSchema = async () => {
+    if (selectedOption !== '') {
+      const fetchedParams = await fetch(`http://localhost:8000/selectModel/${selectedOption}`);
+      const parameterSchema = await fetchedParams.json();
+      setParamSchema(parameterSchema);
+    }
+  };
+  useEffect(() => { getOptions(paramJsonSchema.parent); }, []);
+  useEffect(() => { getParamSchema(); }, [selectedOption]);
+  return (
+    <div key={modelName}>
+      <div>
+        <label htmlFor={modelName}>{modelName}</label>
+        <select value={selectedOption} name="choice" onChange={(e) => setSelectedOption(e.target.value)}>
+          {options.map((option) => <option key={option}>{option}</option>)}
+        </select>
+      </div>
+      <Accordion>
+        <Accordion.Item eventKey="0">
+          <Accordion.Header>{`${selectedOption} parameters`}</Accordion.Header>
+          <Accordion.Body>
+            <SubForm
+              name={modelName}
+              parameterSchema={paramSchema}
+              setFieldValue={setFieldValue}
+              choice={selectedOption}
+              key={`SubForm-${selectedOption}`}
+            />
+          </Accordion.Body>
+        </Accordion.Item>
+      </Accordion>
+    </div>
+  );
+}
 
 const genInput = (modelName, paramJsonSchema, formik) => {
   const { type, properties } = paramJsonSchema;
@@ -90,26 +147,12 @@ const genInput = (modelName, paramJsonSchema, formik) => {
 
     case 'class':
       return (
-        <div key={modelName}>
-          <div>
-            <label htmlFor={modelName}>{modelName}</label>
-            <select>
-              <option>{paramJsonSchema.parent}</option>
-            </select>
-          </div>
-          <Accordion>
-            <Accordion.Item eventKey="0">
-              <Accordion.Header>{`${modelName} parameters`}</Accordion.Header>
-              <Accordion.Body>
-                <SubForm
-                  name={modelName}
-                  parameterSchema={{}}
-                  setFieldValue={formik.setFieldValue}
-                />
-              </Accordion.Body>
-            </Accordion.Item>
-          </Accordion>
-        </div>
+        <ClassInput
+          modelName={modelName}
+          paramJsonSchema={paramJsonSchema}
+          setFieldValue={formik.setFieldValue}
+          key={`rec-param-${modelName}`}
+        />
       );
 
     default:
@@ -142,6 +185,7 @@ function SubForm({
   name,
   parameterSchema,
   setFieldValue,
+  choice,
 }) {
   SubForm.propTypes = {
     name: PropTypes.string,
@@ -153,24 +197,26 @@ function SubForm({
       ]),
     ).isRequired,
     setFieldValue: PropTypes.func.isRequired,
+    choice: PropTypes.string.isRequired,
   };
 
   SubForm.defaultProps = {
     name: 'undefined',
   };
   const defaultValues = getDefaultValues(parameterSchema);
+  const newDefaultValues = { ...defaultValues, choice };
   if (defaultValues === 'null') {
     return (<p>Recursion</p>);
   }
   const formik = useFormik({
-    initialValues: defaultValues,
+    initialValues: newDefaultValues,
   });
   useEffect(() => {
     setFieldValue(name, formik.values);
   }, [formik.values]);
 
   return (
-    <div key={uuid()}>
+    <div key={`parameterForm-${name}`}>
       { genInput(name, parameterSchema, formik) }
     </div>
   );
