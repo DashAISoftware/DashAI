4741b0b498eb036d9fc6330baa6760a3237e9017
maximi4321 <maximiliano.aguilar@ug.uchile.cl>
2022-09-29 23:45:01 -0300

    setup react router dom

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -2,11 +2,15 @@ import React from 'react';
 // import { React, useState, useEffect } from 'react';
 // import logo from './logo.svg';
 import './App.css';
+import { useRoutes } from 'react-router-dom';
 import ExperimentConfiguration from './layouts/ExperimentConfiguration';
+import Results from './layouts/Results';
 
 function App() {
-  return (
-    <ExperimentConfiguration />
-  );
+  const element = useRoutes([
+    { path: '/', element: <ExperimentConfiguration /> },
+    { path: 'results/:sessionId', element: <Results /> },
+  ]);
+  return (element);
 }
 export default App;
