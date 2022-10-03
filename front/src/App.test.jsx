3f2c505e11b7fe18e5bce4abee0fbae5915ccb90
maximi4321 <maximiliano.aguilar@ug.uchile.cl>
2022-10-03 16:42:29 -0300

    fix: pass tests

--- front/src/App.test.jsx
+++ front/src/App.test.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
 // import { render, screen } from '@testing-library/react';
 import { render } from '@testing-library/react';
+import { BrowserRouter as Router } from 'react-router-dom';
 import '@testing-library/jest-dom';
 import App from './App';
 
 test('loads and displays model', async () => {
-  render(<App />);
+  render(
+    <Router>
+      <App />
+    </Router>
+  );
   // expect(screen.getByText('Learn React')).toBeInTheDocument();
 });
