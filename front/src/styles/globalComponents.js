c4dc80189c37fe2dacaa7411c87ccd57b9fb6456
maximi4321 <maximiliano.aguilar@ug.uchile.cl>
2022-10-12 21:01:17 -0300

    add: global theme and global style

---
+++ front/src/styles/globalComponents.js
@@ -0,0 +1,55 @@
+import styled from 'styled-components';
+// import { styled as muiStyled } from '@mui/system';
+import {
+  Button,
+  Card,
+  FloatingLabel,
+  Form,
+} from 'react-bootstrap';
+// import { TextField } from '@mui/material';
+export const StyledButton = styled(Button)`
+  background-color: ${(props) => props.theme.button.background};
+  color: ${(props) => props.theme.button.text};
+  &:hover {
+    background-color: ${(props) => props.theme.button.backgroundHover};
+  }
+`;
+
+export const Title = styled.h2`
+  color: ${(props) => props.theme.title};
+`;
+
+export const SubTitle = styled.h4`
+  color: ${(props) => props.theme.subTitle};
+`;
+
+export const P = styled.p`
+  color: ${(props) => props.theme.simpleText};
+`;
+
+export const StyledCard = styled(Card)`
+  background-color: 'red';
+`;
+
+export const StyledFloatingLabel = styled(FloatingLabel)`
+   color: ${(props) => props.theme.label.text};
+ `;
+
+export const StyledTextInput = styled(Form.Control)`
+ &:not(active){
+   color: ${(props) => props.theme.input.text};
+   background-color: ${(props) => props.theme.rootBackground};
+ }
+ &:focus{
+   color: ${(props) => props.theme.input.text};
+   background-color: ${(props) => props.theme.rootBackground};
+ }
+`;
+
+export const StyledSelect = styled(Form.Select)`
+  color: ${(props) => props.theme.input.text};
+  background-color: ${(props) => props.theme.rootBackground};
+  background-image: url("data:image/svg+xml,%3csvg xmlns=%27http://www.w3.org/2000/svg%27 viewBox=%270 0 16 16%27%3e%3cpath fill=%27white%27 stroke=%27%23white%27 stroke-linecap=%27round%27 stroke-linejoin=%27round%27 stroke-width=%272%27 d=%27m2 5 6 6 6-6%27/%3e%3c/svg%3e") !important;
+  }
+`;
+// export const InputText = muiStyled(TextField)``;
---
+++ front/src/styles/theme.js
@@ -0,0 +1,20 @@
+const theme = {
+  rootBackground: '#2e3037',
+  title: '#fff',
+  subTitle: '#fff',
+  button: {
+    background: '#0cbebb',
+    text: '#fff',
+    backgroundHover: '#097a78',
+  },
+  simpleText: '#fff',
+  input: {
+    text: '#fff',
+  },
+  label: {
+    text: '#d3d3d3',
+  },
+  uploadLabel: '#d3d3d3',
+};
+
+export default theme;
