a738b4d3edb2055fb1ff3119533f84af05a2e85e
maximi4321 <maximiliano.aguilar@ug.uchile.cl>
2022-10-24 16:17:55 -0300

    add: ExperimentPipeline and addModels components

---
+++ front/src/components/AddModels.jsx
@@ -0,0 +1,91 @@
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+import { Form } from 'react-bootstrap';
+import {
+  StyledButton,
+  Title,
+  StyledTextInput,
+  StyledSelect,
+  StyledFloatingLabel,
+} from '../styles/globalComponents';
+import ModelsTable from './ModelsTable';
+import { getFullDefaultValues } from '../utils/values';
+
+function AddModels({
+  availableModels,
+  renderFormFactory,
+  setConfigByTableIndex,
+}) {
+  AddModels.propTypes = {
+    availableModels: PropTypes.arrayOf(PropTypes.string).isRequired,
+    renderFormFactory: PropTypes.func.isRequired,
+    setConfigByTableIndex: PropTypes.func.isRequired,
+  };
+  const [modelsInTable, setModelsInTable] = useState([]);
+  const [addModelValues, setAddModelValues] = useState({ name: '', type: '' });
+  const handleSubmit = async (e) => {
+    e.preventDefault(e);
+    if (addModelValues.type !== '' && addModelValues.type !== 'none') {
+      const index = modelsInTable.length;
+      setModelsInTable([...modelsInTable, addModelValues]);
+      const fetchedJsonSchema = await fetch(`${process.env.REACT_APP_SELECT_MODEL_ENDPOINT + addModelValues.type}`);
+      const parameterSchema = await fetchedJsonSchema.json();
+      const defaultValues = await getFullDefaultValues(parameterSchema);
+      setConfigByTableIndex(index, addModelValues.type, defaultValues);
+    }
+  };
+  const handleChange = (e) => {
+    setAddModelValues((state) => ({
+      ...state,
+      [e.target.name]: e.target.value,
+    }));
+  };
+  if (availableModels.length !== 0) {
+    return (
+      <div>
+        <br />
+        <br />
+        <Title>Add Models</Title>
+        <br />
+        <br />
+        <Form className="d-flex" style={{ display: 'flex', flexDirection: 'column' }}>
+          <div style={{ display: 'flex', flexDirection: 'row', gridGap: '2rem' }}>
+            <StyledFloatingLabel className="mb-3" label="nickname (optional)">
+              <StyledTextInput
+                type="text"
+                name="name"
+                value={addModelValues.name}
+                placeholder="model 1"
+                onChange={handleChange}
+              />
+            </StyledFloatingLabel>
+            <StyledFloatingLabel className="mb-3" label="model type">
+              <StyledSelect
+                value={addModelValues.type}
+                name="type"
+                onChange={handleChange}
+                aria-label="Select a model type"
+              >
+                <option value="none" selected hidden>Select model</option>
+                { availableModels.map(
+                  (model) => <option value={model} key={model}>{model}</option>,
+                )}
+              </StyledSelect>
+            </StyledFloatingLabel>
+          </div>
+          <div>
+            <StyledButton style={{ width: '6.9rem' }} onClick={handleSubmit} variant="dark">Add model</StyledButton>
+          </div>
+        </Form>
+        <br />
+        <ModelsTable
+          rows={modelsInTable}
+          renderFormFactory={renderFormFactory}
+        />
+      </div>
+    );
+  }
+  return (<div />);
+}
+
+export default AddModels;
---
+++ front/src/layouts/ExperimentPipeline.jsx
@@ -0,0 +1,143 @@
+import React, { useState, useRef } from 'react';
+import { Container } from 'react-bootstrap';
+import styled from 'styled-components';
+import Upload from '../components/Upload';
+import ParameterForm from '../components/ParameterForm';
+import { StyledButton, P } from '../styles/globalComponents';
+import AddModels from '../components/AddModels';
+import Results from '../components/Results';
+import Play from '../components/Play';
+
+const Step = styled.div`
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  text-align: center;
+  justify-content: center;
+  display: ${(props) => props.showStep};
+`;
+const Loading = styled.img`
+  @keyframes spin {
+    from {transform:rotate(0deg);}
+    to {transform:rotate(360deg);}
+  }
+  animation: spin 3s linear infinite;
+`;
+
+function Experiment() {
+  const [availableModels, setAvailableModels] = useState([]);
+  const [formData, setFormData] = useState({ type: '', index: -1, parameterSchema: {} });
+  const [executionConfig, setExecutionConfig] = useState({});
+  const [resultsState, setResultsState] = useState('none');
+  const [showStep, setShowStep] = useState(['', '', 'none', 'none']);
+  const [show, setShow] = useState(false);
+  const handleClose = () => setShow(false);
+  const setConfigByTableIndex = (index, modelName, newValues) => setExecutionConfig(
+    {
+      ...executionConfig,
+      [index]: { model_name: modelName, payload: newValues },
+    },
+  );
+  const renderFormFactory = (type, index) => (
+    async () => {
+      const fetchedJsonSchema = await fetch(`${process.env.REACT_APP_SELECT_MODEL_ENDPOINT + type}`);
+      const parameterSchema = await fetchedJsonSchema.json();
+      setFormData({ type, index, parameterSchema });
+      setShow(true);
+    }
+  );
+  const resultsRef = useRef(null);
+  const scrollToResults = () => {
+    resultsRef.current?.scrollIntoView({ behavior: 'smooth' });
+    setShowStep(['', '', '', '']);
+  };
+  const sendModelConfig = async () => {
+    scrollToResults();
+    setResultsState('waiting');
+    const fetchedResults = await fetch(
+      `${process.env.REACT_APP_SELECTED_PARAMETERS_ENDPOINT + executionConfig[0].model_name}`,
+      {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(executionConfig[0].payload),
+      },
+    );
+    const sessionId = await fetchedResults.json();
+    await fetch(
+      `${process.env.REACT_APP_EXPERIMENT_RUN_ENDPOINT + sessionId}`,
+      { method: 'POST' },
+    );
+    setResultsState('ready');
+  };
+  const loadDatasetRef = useRef(null);
+  const addModelsRef = useRef(null);
+  const scrollToAddModel = () => {
+    addModelsRef.current?.scrollIntoView({ behavior: 'smooth' });
+    setShowStep(['', '', '', 'none']);
+  };
+  const playRef = useRef(null);
+  const scrollToPlay = () => {
+    playRef.current?.scrollIntoView({ behavior: 'smooth' });
+  };
+  return (
+    <Container>
+      <Step style={{ height: '89.5vh' }} ref={loadDatasetRef} showStep={showStep[0]}>
+        <Upload
+          setModels={setAvailableModels}
+          scrollToNextStep={scrollToAddModel}
+        />
+      </Step>
+      <Step ref={addModelsRef} showStep={showStep[1]}>
+        <AddModels
+          availableModels={availableModels}
+          renderFormFactory={renderFormFactory}
+          setConfigByTableIndex={setConfigByTableIndex}
+        />
+        <ParameterForm
+          type={formData.type}
+          index={formData.index}
+          parameterSchema={formData.parameterSchema}
+          setConfigByTableIndex={setConfigByTableIndex}
+          defaultValues={executionConfig[formData.index]}
+          modalShow={show}
+          handleClose={handleClose}
+          key={formData.index}
+        />
+        {
+          Object.keys(executionConfig).length > 0
+            && <StyledButton variant="dark" onClick={sendModelConfig}>Run Experiment</StyledButton>
+        }
+      </Step>
+      <Step ref={resultsRef} showStep={showStep[2]}>
+        <div>
+          {
+          resultsState === 'ready'
+          && <Results scrollToNextStep={scrollToPlay} />
+          }
+          {
+          resultsState === 'waiting'
+          && (
+          <div>
+            <P>Loading results...</P>
+            <Loading alt="" src="images/loading.png" width="58" height="58" />
+          </div>
+          )
+          }
+        </div>
+      </Step>
+      <Step ref={playRef} showStep={showStep[3]}>
+        <Play />
+        <br />
+        <StyledButton
+          onClick={() => loadDatasetRef.current?.scrollIntoView({ behavior: 'smooth' })}
+        >
+          Back to Top
+        </StyledButton>
+      </Step>
+    </Container>
+  );
+}
+export default Experiment;
