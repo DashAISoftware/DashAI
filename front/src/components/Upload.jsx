8ee756b79643945ff893d7cf45c6870159ab264f
maximi4321 <maximiliano.aguilar@ug.uchile.cl>
2022-10-24 16:20:35 -0300

    add: icons, animations and buttons

--- front/src/components/Upload.jsx
+++ front/src/components/Upload.jsx
@@ -1,7 +1,12 @@
 import React, { useState, useRef } from 'react';
 import PropTypes from 'prop-types';
 import { useNavigate } from 'react-router-dom';
-import { P } from '../styles/globalComponents';
+import styled from 'styled-components';
+import {
+  P,
+  Title,
+  StyledButton,
+} from '../styles/globalComponents';
 import {
   FormFileUpload,
   LabelFileUpload,
@@ -9,16 +14,27 @@ import {
   DragFile,
 } from '../styles/components/UploadStyles';
 
-function Upload({ setModels, setTaskName, setShowUpload }) {
+const Uploading = styled.img`
+  @keyframes spin {
+    from {transform:rotate(0deg);}
+    to {transform:rotate(360deg);}
+  }
+  animation: spin 3s linear infinite;
+`;
+
+function Upload({ setModels, scrollToNextStep }) {
   Upload.propTypes = {
     setModels: PropTypes.func.isRequired,
-    setTaskName: PropTypes.func.isRequired,
-    setShowUpload: PropTypes.func.isRequired,
+    scrollToNextStep: PropTypes.func.isRequired,
   };
   const navigate = useNavigate();
+  // const { empty, loading, loaded } = [0, 1, 2];
+  const [datasetState, setDatasetState] = useState(0);
+  const [taskName, setTaskName] = useState('');
   const [dragActive, setDragActive] = useState(false);
   const inputRef = useRef(null);
   const uploadFile = async (file) => {
+    setDatasetState(1);
     const formData = new FormData();
     formData.append('file', file);
     try {
@@ -31,7 +47,7 @@ function Upload({ setModels, setTaskName, setShowUpload }) {
       const fetchedTask = await fetch(`${process.env.REACT_APP_TASK_NAME_ENDPOINT + sessionId}`);
       const task = await fetchedTask.json();
       setTaskName(task);
-      setShowUpload(false);
+      setDatasetState(2);
       if (typeof models.error !== 'undefined') {
         navigate('/error');
       } else {
@@ -64,14 +80,57 @@ function Upload({ setModels, setTaskName, setShowUpload }) {
   const handleButtonClick = () => {
     inputRef.current.click();
   };
+  const stateText = (state) => {
+    switch (state) {
+      case (0): return ('Upload your dataset');
+      case (1): return ('Loading dataset');
+      case (2): return ('Uploaded dataset');
+      default: return ('');
+    }
+  };
+  const stateImg = (state) => {
+    switch (state) {
+      case (0):
+        return (
+          <div>
+            <input ref={inputRef} id="input-upload-dataset" style={{ display: 'none' }} type="file" onChange={handleSelect} />
+            <p>Drag and drop your file here or</p>
+            <UploadButton type="button" onClick={handleButtonClick}>Upload a file</UploadButton>
+          </div>
+        );
+      case (1):
+        return (
+          <Uploading
+            alt=""
+            src="images/loading.png"
+            width="58"
+            height="58"
+          />
+        );
+      case (2):
+        return (
+          <img
+            alt=""
+            src="images/loaded.png"
+            width="58"
+            height="58"
+          />
+        );
+      default:
+        return (<div />);
+    }
+  };
   return (
     <div>
-      <P>Upload your dataset</P>
+      <Title>Load Dataset</Title>
+      <br />
+      <br />
+      <P>
+        {stateText(datasetState)}
+      </P>
       <FormFileUpload onDragEnter={handleDrag}>
         <LabelFileUpload htmlFor="input-upload-dataset" className={dragActive ? 'drag-active' : ''}>
-          <input ref={inputRef} id="input-upload-dataset" style={{ display: 'none' }} type="file" onChange={handleSelect} />
-          <p>Drag and drop your file here or</p>
-          <UploadButton type="button" onClick={handleButtonClick}>Upload a file</UploadButton>
+          { stateImg(datasetState) }
         </LabelFileUpload>
         { dragActive
           && (
@@ -83,6 +142,12 @@ function Upload({ setModels, setTaskName, setShowUpload }) {
             />
           )}
       </FormFileUpload>
+      <br />
+      { taskName !== ''
+        && <P style={{ fontSize: '18px', lineHeight: '22.5px' }}>{`Task Type: ${taskName}`}</P>}
+      <br />
+      { datasetState === 2
+      && <StyledButton type="button" onClick={scrollToNextStep}>Next</StyledButton>}
     </div>
   );
 }
