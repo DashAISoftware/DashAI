6c7ae0f96c48bd619cd2ebfdae75441868fceca0
maximi4321 <maximiliano.aguilar@ug.uchile.cl>
2022-11-01 17:48:36 -0300

    add: verify if API is online

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+// import { Container } from 'react-bootstrap';
 // import { React, useState, useEffect } from 'react';
 // import logo from './logo.svg';
 import './App.css';
@@ -9,15 +10,42 @@ import Error from './components/Error';
 import Navbar from './components/Navbar';
 
 function App() {
+  const [apiIsOnline, setApiIsOnline] = useState(true);
+  useEffect(
+    () => {
+      async function apiOnlineTest() {
+        try {
+          const fetchApiState = await fetch(`${process.env.REACT_APP_STATE_ENDPOINT}`);
+          const apiState = await fetchApiState.json();
+          setApiIsOnline(apiState.state === 'online');
+        } catch (e) {
+          setApiIsOnline(false);
+        }
+      }
+      apiOnlineTest();
+    },
+    [],
+  );
   const element = useRoutes([
     { path: '/', element: <ExperimentPipeline /> },
     // { path: 'results/:sessionId', element: <Results /> },
-    { path: 'error', element: <Error /> },
+    // { path: 'error', element: <Error /> },
   ]);
   return (
     <div>
       <Navbar />
-      { element }
+      {apiIsOnline
+        ? element
+        : (
+          <div style={{
+            marginLeft: '30.3vw',
+            marginTop: '30vh',
+            textAlign: 'center',
+          }}
+          >
+            <Error message="API is not online" />
+          </div>
+        )}
     </div>
   );
 }
