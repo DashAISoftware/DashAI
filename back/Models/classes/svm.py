0978f48a055ea49eded1ebcb24e08dd0fa9acbb4
Cristian Tamblay <30818944+cristian-tamblay@users.noreply.github.com>
2022-11-22 17:23:28 -0300

    Revert "Back/initial db"

--- back/Models/classes/svm.py
+++ back/Models/classes/svm.py
@@ -3,10 +3,10 @@ import json
 from sklearn.svm import SVC
 
 from Models.classes.numericClassificationModel import NumericClassificationModel
-from Models.classes.sklearnLikeModel import SklearnLikeModel
+from Models.classes.sklearnLikeModel import SkleanLikeModel
 
 
-class SVM(SklearnLikeModel, NumericClassificationModel, SVC):
+class SVM(SkleanLikeModel, NumericClassificationModel, SVC):
     """
     Support vector machine. Supervised learning algorithm that separates
     two classes in two spaces by means of a hyperplane. This hyperplane is
--- back/db.py
+++ back/db.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-engine = create_engine("sqlite:///back/Database/dashAI.sqlite")
+engine = create_engine("sqlite:///dashAI.sqlite")
 Session = sessionmaker(bind=engine)
 session = Session()
 
