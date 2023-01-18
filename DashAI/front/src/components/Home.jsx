f14f5fd90c1a3aac30396e28fc369c2813c8f6dc
maximi4321 <maximiliano.aguilar@ug.uchile.cl>
2023-01-18 15:44:26 -0300

    add Home component to select previous experiments

---
+++ DashAI/front/src/components/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Container } from 'react-bootstrap';
+
+function Home() {
+  return (
+    <Container>
+      <p style={{ color: '#fff' }}>I am a new page</p>
+    </Container>
+  );
+}
+
+export default Home;
