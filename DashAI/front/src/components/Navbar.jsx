af25053deaedecb4406644eb83df7b5a3ced72e6
maximi4321 <maximiliano.aguilar@ug.uchile.cl>
2023-01-18 15:44:04 -0300

    Navbar changes to manage tabs

--- DashAI/front/src/components/Navbar.jsx
+++ DashAI/front/src/components/Navbar.jsx
@@ -1,17 +1,27 @@
 import React from 'react';
 import { Container } from 'react-bootstrap';
+import { useActiveTab } from '../context/TabsProvider';
 import * as S from '../styles/components/NavbarStyles';
 
 function Navbar() {
+  const { setActiveTab } = useActiveTab();
   return (
     <S.Navbar>
       <Container>
-        <S.Navbar.Brand href="#home">
+        <S.Navbar.Brand onClick={() => setActiveTab('home')} style={{ cursor: 'pointer' }}>
           <S.Logo
             alt=""
             src="images/logo.png"
           />
         </S.Navbar.Brand>
+        <S.Navbar.Collapse>
+          <S.Nav>
+            <S.Nav.Link onClick={() => setActiveTab('data')}>Data</S.Nav.Link>
+            <S.Nav.Link onClick={() => setActiveTab('experiment')}>Experiment</S.Nav.Link>
+            <S.Nav.Link onClick={() => setActiveTab('results')}>Results</S.Nav.Link>
+            <S.Nav.Link onClick={() => setActiveTab('play')}>Play</S.Nav.Link>
+          </S.Nav>
+        </S.Navbar.Collapse>
       </Container>
     </S.Navbar>
   );
