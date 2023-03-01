cc363a4112933be9b0ed16a7d5273d5ec41874b2
Cristian Tamblay <cristian.tamblay@ing.uchile.cl>
2023-03-01 17:19:43 -0300

    Pre-commit fix

--- DashAI/back/database/db.py
+++ DashAI/back/database/db.py
@@ -1,5 +1,5 @@
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, declarative_base
+from sqlalchemy.orm import declarative_base, sessionmaker
 
 from DashAI.back.config import settings
 
