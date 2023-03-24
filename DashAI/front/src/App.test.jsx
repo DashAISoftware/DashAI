19f355ebdf62225eea52c5ee42cfbe8a79b996d4
maximi4321 <maximiliano.aguilar@ug.uchile.cl>
2023-03-24 13:55:16 -0300

    add CssBaseline

--- DashAI/front/src/App.test.jsx
+++ DashAI/front/src/App.test.jsx
@@ -3,16 +3,20 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
 import '@testing-library/jest-dom';
 import App from './App';
 import theme from './styles/theme';
+import muiGlobalStyle from './styles/muiGlobalStyles';
 
-const myTheme = createTheme(theme);
+
+const myTheme = createTheme({ ...theme, ...muiGlobalStyle });
 
 test('loads and displays model', async () => {
   render(
     <Router>
       <ThemeProvider theme={myTheme}>
+        <CssBaseline />
         <App />
       </ThemeProvider>
     </Router>
--- DashAI/front/src/index.jsx
+++ DashAI/front/src/index.jsx
@@ -3,19 +3,22 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import theme from './styles/theme';
 import GlobalStyle from './styles/globalStyles';
+import muiGlobalStyle from './styles/muiGlobalStyles';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-const myTheme = createTheme(theme);
+const myTheme = createTheme({ ...theme, ...muiGlobalStyle });
 root.render(
   <React.StrictMode>
     <Router>
       <GlobalStyle />
       <ThemeProvider theme={myTheme}>
+        <CssBaseline />
         <App />
       </ThemeProvider>
     </Router>
