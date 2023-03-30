79e618ca05587a4ad1f465dd9968786235078c78
Cristian Tamblay <cristian.tamblay@ing.uchile.cl>
2023-03-30 11:41:30 -0300

    Fixed datetime creating NULL default and modified, added more tests

--- tests/back/api/test_datasets.py
+++ tests/back/api/test_datasets.py
@@ -28,12 +28,38 @@ def test_create_csv_dataset(client):
         "url" : ""},
         files={"file": ("filename", csv, "text/csv")}
     )
+    response = client.post(
+        "/api/v1/dataset/",
+        data = {"params": '''{  "task_name": "TabularClassificationTask",
+                                "dataloader": "CSVDataLoader",
+                                "dataset_name": "test_csv2",
+                                "class_column": -1,
+                                "splits_in_folders": false,
+                                "splits": {
+                                    "train_size": 0.5,
+                                    "test_size": 0.2,
+                                    "val_size": 0.3,
+                                    "seed": 42,
+                                    "shuffle": true,
+                                    "stratify": false
+                                },
+                                "dataloader_params": {
+                                    "separator": ","
+                                }
+                            }''',
+        "url" : ""},
+        files={"file": ("filename", csv, "text/csv")}
+    )
     assert response.status_code == 201, response.text
     response = client.get("/api/v1/dataset/1")
     assert response.status_code == 200, response.text
     data = response.json()
     assert data["name"] == "test_csv"
     assert data["task_name"] == "TabularClassificationTask"
+    response = client.get("/api/v1/dataset/2")
+    assert response.status_code == 200, response.text
+    data = response.json()
+    assert data["name"] == "test_csv2"
 
 def test_get_all_datasets(client):
     response = client.get("/api/v1/dataset/")
@@ -41,18 +67,19 @@ def test_get_all_datasets(client):
     data = response.json()
     assert data[0]["name"] == "test_csv"
     assert data[0]["task_name"] == "TabularClassificationTask"
+    assert data[1]["name"] == "test_csv2"
 
 def test_get_wrong_dataset(client):
-    response = client.get("/api/v1/dataset/2")
+    response = client.get("/api/v1/dataset/31415")
     assert response.status_code == 404, response.text
     assert response.text == '{"detail":"Dataset not found"}'
 
 def test_modify_dataset(client):
-    response = client.put("/api/v1/dataset/1",
+    response = client.put("/api/v1/dataset/2",
                           params={"name":"test_modify_name",
                                   "task_name":"UnknownTask"})
     assert response.status_code == 200, response.text
-    response = client.get("/api/v1/dataset/1")
+    response = client.get("/api/v1/dataset/2")
     assert response.status_code == 200, response.text
     data = response.json()
     assert data["name"] == "test_modify_name"
@@ -62,4 +89,4 @@ def test_modify_dataset(client):
 def test_delete_dataset(client):
     response = client.delete("/api/v1/dataset/1")
     assert response.status_code == 204, response.text
-    os.remove("tests/back/test.sqlite")
+
--- tests/back/conftest.py
+++ tests/back/conftest.py
@@ -1,4 +1,5 @@
 import os
+import shutil
 import pytest
 from fastapi.testclient import TestClient
 from sqlalchemy import create_engine
@@ -7,11 +8,18 @@ from DashAI.back.main import api_v1, app
 from DashAI.back.database.models import Base
 from DashAI.back.api.deps import get_db
 
+USER_DATASETS_PATH = "DashAI/back/user_datasets"
+TEST_DB_PATH = "tests/back/test.sqlite"
 
 @pytest.fixture(scope="session", autouse=True)
 def setup_and_delete_db():
-    TEST_DB_PATH = "sqlite:///tests/back/test.sqlite"
-    engine = create_engine(TEST_DB_PATH)
+    try:
+        shutil.rmtree(f"{USER_DATASETS_PATH}/test_csv", ignore_errors=True)
+        shutil.rmtree(f"{USER_DATASETS_PATH}/test_csv2", ignore_errors=True)
+        os.remove(TEST_DB_PATH)
+    except OSError:
+        pass
+    engine = create_engine(f"sqlite:///{TEST_DB_PATH}")
     TestingSessionLocal = sessionmaker(bind=engine)
     Base.metadata.create_all(bind=engine)
     def db():
@@ -22,8 +30,8 @@ def setup_and_delete_db():
             db.close()
     api_v1.dependency_overrides[get_db] = db
     yield
-    if os.path.isfile(TEST_DB_PATH):
-        os.remove(TEST_DB_PATH)
+    shutil.rmtree(f"{USER_DATASETS_PATH}/test_csv", ignore_errors=True)
+    shutil.rmtree(f"{USER_DATASETS_PATH}/test_csv2", ignore_errors=True)
 
 
 @pytest.fixture(scope="module")
