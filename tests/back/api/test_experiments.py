19c3c1082999e160c66f6fa226d1f88085f44b67
Lurrea <rodrigo.urrea@ug.uchile.cl>
2023-04-09 14:11:50 -0400

    add test file for experiment endpoint

--- tests/back/api/test_experiments.py
+++ tests/back/api/test_experiments.py
@@ -1,57 +1,117 @@
 import pytest
 from DashAI.back.database.models import Dataset
+from fastapi.testclient import TestClient
+from sqlalchemy.orm import Session
 
-@pytest.fixture(scope="module")
-def setup_experiments():
+from DashAI.back.api.deps import get_db
+from fastapi import Depends
+
+
+@pytest.fixture(scope="module", name="dummy_dataset_id")
+def fixture_dummy_dataset_id(db: Session = Depends(get_db)):
     # Create Dummy Dataset
-    # - Create the dummy dataset directly to the DB
-    dataset_id = None
-    yield dataset_id
+    dummy_dataset = Dataset(
+        name="DummyDataset",
+        task_name="TabularClassificationTask",
+        file_path="dummy.csv",
+    )
+    db.add(dummy_dataset)
+    db.commit()
+    db.refresh(dummy_dataset)
+    print(dummy_dataset.id)
+    yield dummy_dataset.id
+
     # Delete the dataset
-    # - delete the dataset from the test db
+    db.delete(dummy_dataset)
+    db.commit()
 
-def test_create_experiment(client):
+
+def test_create_experiment(client: TestClient, dummy_dataset_id: int):
     # Create Experiment using the dummy dataset
-    # - Set the dataset of the experiment as dummy dataset
-    # - (Optional) Set the task_name as TabularClassificationTask
+    response = client.post(
+        "/api/v1/experiment/",
+        data={
+            "params": f"""{{  "dataset_id": {dummy_dataset_id},
+                                "task_name": "TabularClassificationTask",
+                            }}""",
+            "url": "",
+        },
+    )
+    assert response.status_code == 201, response.text
 
-    # Create Experiment using the dummy dataset
-    # - Set the dataset of the experiment as dummy dataset
-    # - (Optional) Set the task_name as TabularClassificationTask
+    response = client.post(
+        "/api/v1/experiment/",
+        data={
+            "params": f"""{{  "dataset_id": {dummy_dataset_id},
+                                "task_name": "TabularClassificationTask",
+                            }}""",
+            "url": "",
+        },
+    )
+    assert response.status_code == 201, response.text
 
-    # Check the params of the both experiments
-    # - id -> default id
-    # - dataset_id -> equal to dummy dataset id
-    # - task_name -> equal to input task_name
-    # - step -> inital step or TASK_SELECTION
-    # - created = last_modified = now
-    # - runs -> Empty list
-    pass
+    response = client.get("/api/v1/experiment/1")
+    assert response.status_code == 200, response.text
+    data = response.json()
+    assert data["dataset_id"] == dummy_dataset_id
+    assert data["task_name"] == "TabularClassificationTask"
+    assert data["step"] == "NOT_STARTED"
+    assert data["runs"] == []
 
-def test_get_all_experiments(client):
+    response = client.get("/api/v1/experiment/2")
+    assert response.status_code == 200, response.text
+    data = response.json()
+    assert data["dataset_id"] == dummy_dataset_id
+    assert data["task_name"] == "TabularClassificationTask"
+    assert data["step"] == "NOT_STARTED"
+    assert data["runs"] == []
+
+
+def test_get_all_experiments(client: TestClient, dummy_dataset_id: int):
     # Get all the experiments available in the back
-    # - Check both experiment were uploaded succesfully
-    pass
+    response = client.get("/api/v1/experiment/2")
+    assert response.status_code == 200, response.text
+    data = response.json()
+    assert data[0]["dataset_id"] == dummy_dataset_id
+    assert data[1]["dataset_id"] == dummy_dataset_id
 
-def test_get_wrong_experiment(client):
+
+def test_get_wrong_experiment(client: TestClient):
     # Try to retrieve a non-existent experiment an get an error
-    pass
+    response = client.get("/api/v1/experiment/31415")
+    assert response.status_code == 404, response.text
+    assert response.text == '{"detail":"Dataset not found"}'
 
-def test_modify_experiment(client):
-    # Modify an existen experiment
-    # - modify the task_name
-    # - (Optional) modify the actual step
+
+def test_modify_experiment(client: TestClient, dummy_dataset_id: int):
+    # Modify an existent experiment
+    response = client.patch(
+        "/api/v1/experiment/2",
+        data={"params": """{"task_name": "UnknownTask"}""", "url": ""},
+    )
+    assert response.status_code == 201, response.text
 
     # Get the experiment
-    # - check the dataset is didn't change
-    # - check the task_name changes
-    # - (optional) check the actual step changes
-    # - check the last_modified changes to now
-    pass
+    response = client.get("/api/v1/experiment/2")
+    assert response.status_code == 200, response.text
+    data = response.json()
+    assert data["dataset_id"] == dummy_dataset_id
+    assert data["task_name"] == "UnknownTask"
+    assert data["step"] == "NOT_STARTED"
+    assert data["runs"] == []
+    assert data["created"] != data["last_modified"]
 
 
-def test_delete_experiment(client):
+def test_modify_experiment_step(client: TestClient):
+    response = client.patch(
+        "/api/v1/experiment/2",
+        data={"params": """{"step": "STARTED"}""", "url": ""},
+    )
+    assert response.status_code == 403, response.text
+    assert response.text == '{"detail":"Can not modify experiment step"}'
+
+
+def test_delete_experiment(client: TestClient):
     # Delete the first experiment in the db
-    # Check the status code
-    # Get all dataset and check it isn't in the data
-    pass
+    response = client.delete("/api/v1/dataset/1")
+    assert response.status_code == 204, response.text
