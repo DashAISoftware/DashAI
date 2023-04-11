35ca6d82e3ea5fdc6c10db14df0a3f5315eb90b0
Lurrea <rodrigo.urrea@ug.uchile.cl>
2023-04-11 09:46:30 -0400

    create fixture to get the actual session to connect to the db

--- tests/back/conftest.py
+++ tests/back/conftest.py
@@ -11,20 +11,25 @@ from DashAI.back.api.deps import get_db
 USER_DATASETS_PATH = "DashAI/back/user_datasets"
 TEST_DB_PATH = "tests/back/test.sqlite"
 
+@pytest.fixture(scope="session")
+def session():
+    engine = create_engine(f"sqlite:///{TEST_DB_PATH}")
+    TestingSessionLocal = sessionmaker(bind=engine)
+    Base.metadata.create_all(bind=engine)
+    return TestingSessionLocal
+
 @pytest.fixture(scope="session", autouse=True)
-def setup_and_delete_db():
+def setup_and_delete_db(session: sessionmaker):
     try:
         shutil.rmtree(f"{USER_DATASETS_PATH}/test_csv", ignore_errors=True)
         shutil.rmtree(f"{USER_DATASETS_PATH}/test_csv2", ignore_errors=True)
         os.remove(TEST_DB_PATH)
     except OSError:
         pass
-    engine = create_engine(f"sqlite:///{TEST_DB_PATH}")
-    TestingSessionLocal = sessionmaker(bind=engine)
-    Base.metadata.create_all(bind=engine)
+    
     def db():
         try:
-            db = TestingSessionLocal()
+            db = session()
             yield db
         finally:
             db.close()
