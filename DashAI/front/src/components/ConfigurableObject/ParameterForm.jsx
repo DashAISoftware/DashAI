ddd777b94dbf0453653b60e60685f446f7595044
maximi4321 <maximiliano.aguilar@ug.uchile.cl>
2023-04-12 18:59:41 -0400

    new ParameterForm

---
+++ DashAI/front/src/components/ConfigurableObject/ParameterForm.jsx
@@ -0,0 +1,41 @@
+import React from "react";
+import PropTypes from "prop-types";
+import { Stack } from "@mui/material";
+import { useFormik } from "formik";
+import { genInput } from "./FormInputs";
+
+function ParameterForm({ parameterSchema, defaultValues, onFormSubmit }) {
+  const formik = useFormik({
+    initialValues: defaultValues ?? {},
+    //   validationSchema: getValidation(parameterSchema),
+    onSubmit: (values) => {
+      onFormSubmit(values);
+    },
+  });
+  return (
+    <Stack direction="column" onChange={formik.handleSubmit}>
+      {genInput("KNeighborsClassifier", parameterSchema, formik, defaultValues)}
+    </Stack>
+  );
+}
+
+ParameterForm.propTypes = {
+  parameterSchema: PropTypes.objectOf(
+    PropTypes.oneOfType([PropTypes.string, PropTypes.bool, PropTypes.object])
+  ).isRequired,
+  defaultValues: PropTypes.objectOf(
+    PropTypes.oneOfType([
+      PropTypes.string,
+      PropTypes.bool,
+      PropTypes.number,
+      PropTypes.object,
+    ])
+  ),
+  onFormSubmit: PropTypes.func.isRequired,
+};
+
+ParameterForm.defaultProps = {
+  defaultValues: { emptyDefaultValues: true },
+};
+
+export default ParameterForm;
