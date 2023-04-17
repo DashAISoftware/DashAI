5ae305b05cb9b3841e00535c9865ab5286578a6e
Cristian Tamblay <cristian.tamblay@ing.uchile.cl>
2023-04-17 18:03:49 -0400

    Fixed date code

--- DashAI/front/src/utils/index.js
+++ DashAI/front/src/utils/index.js
@@ -2,9 +2,5 @@ export const formatDate = (date) => {
   if (date == null) {
     return "";
   }
-
-  const formattedDate =
-    // date.getDate() + "/" + (date.getMonth() + 1) + "/" + date.getFullYear();
-    date;
-  return formattedDate;
+  return new Date(date).toLocaleDateString();
 };
