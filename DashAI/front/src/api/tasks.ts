4308fe247d1422bbb7cb7e46927cd6144aa18e79
Pablo Badilla <pbadilla.torrealba@gmail.com>
2023-05-03 14:57:39 -0400

    Delete api/tasks.ts (leave api/task.ts as default task api)

--- DashAI/front/src/api/tasks.ts
+++
@@ -1,7 +0,0 @@
-import type { ITask } from "../types/task";
-import api from "./api";
-
-export const getTasks = async (): Promise<ITask[]> => {
-  const response = await api.get<ITask[]>("/v1/task");
-  return response.data;
-};
