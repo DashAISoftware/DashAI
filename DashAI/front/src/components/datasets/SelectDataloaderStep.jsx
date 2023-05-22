fae5019070e6a9d0913626c20aedb03e67f448a4
maximi4321 <maximiliano.aguilar@ug.uchile.cl>
2023-05-22 16:58:29 -0400

    create a component for each step of the modal

---
+++ DashAI/front/src/components/datasets/SelectDataloaderStep.jsx
@@ -0,0 +1,94 @@
+import React, { useEffect, useState } from "react";
+import PropTypes from "prop-types";
+import { useSnackbar } from "notistack";
+import { getCompatibleDataloaders as getCompatibleDataloadersRequest } from "../../api/dataloader";
+import ItemSelector from "./ItemSelector";
+import { DialogContentText, Paper } from "@mui/material";
+
+/**
+ * This component renders a list of dataloaders and allows the user to select one.
+ * @param {object} newDataset An object that stores all the important states for the dataset modal.
+ * @param {function} setNewDataset function that modifies newDataset state
+ * @param {function} setNextEnabled function to enable or disable the "Next" button in the dataset modal.
+ */
+function SelectDataloaderStep({ newDataset, setNewDataset, setNextEnabled }) {
+  const { enqueueSnackbar } = useSnackbar();
+
+  const [dataloaders, setDataloaders] = useState([]);
+  const [selectedDataloader, setSelectedDataloader] = useState({});
+  const [loading, setLoading] = useState(true);
+
+  async function getCompatibleDataloaders() {
+    setLoading(true);
+    try {
+      const dataloaders = await getCompatibleDataloadersRequest(
+        newDataset.task_name,
+      );
+      setDataloaders(dataloaders);
+      if (newDataset.dataloader !== "") {
+        const previouslySelectedDataloader = dataloaders.find(
+          (dataloader) => dataloader.class === newDataset.dataloader,
+        );
+        setSelectedDataloader(previouslySelectedDataloader);
+      }
+    } catch (error) {
+      enqueueSnackbar("Error while trying to obtain compatible dataloaders", {
+        variant: "error",
+        anchorOrigin: {
+          vertical: "top",
+          horizontal: "right",
+        },
+      });
+      if (error.response) {
+        console.error("Response error:", error.message);
+      } else if (error.request) {
+        console.error("Request error", error.request);
+      } else {
+        console.error("Unkown Error", error.message);
+      }
+    } finally {
+      setLoading(false);
+    }
+  }
+
+  // updates the modal state with the name of the dataloader that is selected by the user
+  useEffect(() => {
+    if ("class" in selectedDataloader) {
+      setNewDataset({ ...newDataset, dataloader: selectedDataloader.class });
+      setNextEnabled(true);
+    }
+  }, [selectedDataloader]);
+
+  // fetches the available dataloaders
+  useEffect(() => {
+    getCompatibleDataloaders();
+  }, []);
+  return (
+    <Paper variant="outlined" sx={{ p: 4 }}>
+      {/* Title */}
+      <DialogContentText
+        sx={{ mb: 3 }}
+      >{`Select a way to upload your data`}</DialogContentText>
+
+      {/* List of dataloaders */}
+      {!loading && (
+        <ItemSelector
+          itemsList={dataloaders}
+          selectedItem={selectedDataloader}
+          setSelectedItem={setSelectedDataloader}
+        />
+      )}
+    </Paper>
+  );
+}
+
+SelectDataloaderStep.propTypes = {
+  newDataset: PropTypes.shape({
+    task_name: PropTypes.string,
+    dataloader: PropTypes.string,
+  }).isRequired,
+  setNewDataset: PropTypes.func.isRequired,
+  setNextEnabled: PropTypes.func.isRequired,
+};
+
+export default SelectDataloaderStep;
---
+++ DashAI/front/src/components/datasets/SelectTaskStep.jsx
@@ -0,0 +1,91 @@
+import React, { useEffect, useState } from "react";
+import PropTypes from "prop-types";
+import { useSnackbar } from "notistack";
+import { getTasks as getTasksRequest } from "../../api/task";
+import ItemSelector from "./ItemSelector";
+import { DialogContentText, Paper } from "@mui/material";
+
+/**
+ * This component renders a list of tasks and allows the user to select one.
+ * @param {object} newDataset An object that stores all the important states for the dataset modal.
+ * @param {function} setNewDataset function that modifies newDataset state
+ * @param {function} setNextEnabled function to enable or disable the "Next" button in the dataset modal.
+ */
+function SelectTaskStep({ newDataset, setNewDataset, setNextEnabled }) {
+  const { enqueueSnackbar } = useSnackbar();
+
+  const [tasks, setTasks] = useState([]);
+  const [selectedTask, setSelectedTask] = useState({});
+  const [loading, setLoading] = useState(true);
+
+  const getTasks = async () => {
+    setLoading(true);
+    try {
+      const tasks = await getTasksRequest();
+      setTasks(tasks);
+      // If there was a previously selected task, it is used as the initial value for the task.
+      if (newDataset.task_name !== "") {
+        const previouslySelectedTask = tasks.find(
+          (task) => task.class === newDataset.task_name,
+        );
+        setSelectedTask(previouslySelectedTask);
+      }
+    } catch (error) {
+      enqueueSnackbar("Error while trying to obtain available tasks", {
+        variant: "error",
+        anchorOrigin: {
+          vertical: "top",
+          horizontal: "right",
+        },
+      });
+      if (error.response) {
+        console.error("Response error:", error.message);
+      } else if (error.request) {
+        console.error("Request error", error.request);
+      } else {
+        console.error("Unkown Error", error.message);
+      }
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  // updates the modal state with the name of the task that is selected by the user
+  useEffect(() => {
+    if ("class" in selectedTask) {
+      setNewDataset({ ...newDataset, task_name: selectedTask.class });
+      setNextEnabled(true);
+    }
+  }, [selectedTask]);
+
+  // fetches the available tasks
+  useEffect(() => {
+    getTasks();
+  }, []);
+  return (
+    <Paper variant="outlined" sx={{ p: 4 }}>
+      {/* Title */}
+      <DialogContentText sx={{ mb: 3 }}>{`Select a task`}</DialogContentText>
+
+      {/* List of tasks  */}
+      {!loading && (
+        <ItemSelector
+          itemsList={tasks}
+          selectedItem={selectedTask}
+          setSelectedItem={setSelectedTask}
+        />
+      )}
+    </Paper>
+  );
+}
+
+SelectTaskStep.propTypes = {
+  newDataset: PropTypes.shape({
+    task_name: PropTypes.string,
+    dataloader_name: PropTypes.string,
+  }).isRequired,
+  setNewDataset: PropTypes.func.isRequired,
+  setNextEnabled: PropTypes.func.isRequired,
+};
+
+export default SelectTaskStep;
