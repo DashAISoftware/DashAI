6f9cc1e221913823e5b65ebb16d117483e93581f
Lurrea <rodrigo.urrea@ug.uchile.cl>
2023-05-23 11:14:09 -0400

    use real dataset for test

--- tests/back/api/test_runner.py
+++ tests/back/api/test_runner.py
@@ -1,28 +1,57 @@
-import pytest
+import os
 from typing import List
+
+import pytest
 from fastapi.testclient import TestClient
 from sqlalchemy.orm import sessionmaker
 
-from DashAI.back.database.models import Dataset, Experiment, Run
+from DashAI.back.database.models import Experiment, Run
+
+
+@pytest.fixture(scope="module", name="dataset_id")
+def fixture_dataset_id(client: TestClient):
+    script_dir = os.path.dirname(__file__)
+    test_dataset = "iris.csv"
+    abs_file_path = os.path.join(script_dir, test_dataset)
+    csv = open(abs_file_path, "rb")
+    response = client.post(
+        "/api/v1/dataset/",
+        data={
+            "params": """{  "task_name": "TabularClassificationTask",
+                                "dataloader": "CSVDataLoader",
+                                "dataset_name": "test_csv",
+                                "outputs_columns": [],
+                                "splits_in_folders": false,
+                                "splits": {
+                                    "train_size": 0.8,
+                                    "test_size": 0.1,
+                                    "val_size": 0.1,
+                                    "seed": 42,
+                                    "shuffle": true,
+                                    "stratify": false
+                                },
+                                "dataloader_params": {
+                                    "separator": ","
+                                }
+                            }""",
+            "url": "",
+        },
+        files={"file": ("filename", csv, "text/csv")},
+    )
+    assert response.status_code == 201, response.text
+    data = response.json()
+    yield data["id"]
+    response = client.delete(f"/api/v1/dataset/{data['id']}")
+    assert response.status_code == 204, response.text
 
 
 @pytest.fixture(scope="module", name="run_id_list")
-def fixture_run_id_list(session: sessionmaker):
+def fixture_run_id_list(session: sessionmaker, dataset_id: int):
     db = session()
-
-    # Create Dummy Dataset
-    dummy_dataset = Dataset(
-        name="DummyDataset",
-        task_name="TabularClassificationTask",
-        file_path="dummy.csv",
-    )
-    db.add(dummy_dataset)
-    db.commit()
-    db.refresh(dummy_dataset)
 
     # Create Dummy Experiment
     dummy_experiment = Experiment(
-        dataset_id=dummy_dataset.id,
+        dataset_id=dataset_id,
         task_name="TabularClassificationTask",
         name="Test Experiment",
     )
@@ -32,7 +61,7 @@ def fixture_run_id_list(session: sessionmaker):
 
     # Create Dummy Runs
     dummy_run1 = Run(
-        experiment_id=dummy_dataset.id,
+        experiment_id=dummy_experiment.id,
         model_name="KNeighborsClassifier",
         parameters={
             "n_neighbors": 5,
@@ -44,7 +73,7 @@ def fixture_run_id_list(session: sessionmaker):
     )
     db.add(dummy_run1)
     dummy_run2 = Run(
-        experiment_id=dummy_dataset.id,
+        experiment_id=dummy_experiment.id,
         model_name="KNeighborsClassifier",
         parameters={
             "n_neighbors": 1,
@@ -63,7 +92,6 @@ def fixture_run_id_list(session: sessionmaker):
 
     # Delete the dataset, experiment and runs
     db.delete(dummy_experiment)
-    db.delete(dummy_dataset)
     db.delete(dummy_run1)
     db.delete(dummy_run2)
     db.commit()
@@ -74,7 +102,7 @@ def test_exec_runs(client: TestClient, run_id_list: List[int]):
         response = client.post(f"/api/v1/runner/?run_id={run_id}")
         assert response.status_code == 200, response.text
     for run_id in run_id_list:
-        response = client.get(f'/api/api_v1/run/?run_id={run_id}')
+        response = client.get(f"/api/api_v1/run/?run_id={run_id}")
         data = response.json()
         assert data["train_metrics"] is not None
         assert data["test_metrics"] is not None
@@ -84,6 +112,6 @@ def test_exec_runs(client: TestClient, run_id_list: List[int]):
 
 
 def test_exec_wrong_run(client: TestClient):
-    response = client.get("/api/v1/runner/?run_id=31415")
+    response = client.post("/api/v1/runner/?run_id=31415")
     assert response.status_code == 404, response.text
     assert response.text == '{"detail":"Run not found"}'
