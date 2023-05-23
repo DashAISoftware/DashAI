f25fa3ae6f318889328363fc6158bdd901656ec5
Lurrea <rodrigo.urrea@ug.uchile.cl>
2023-05-23 11:13:46 -0400

    fix datasets paths

--- DashAI/back/api/api_v1/endpoints/runner.py
+++ DashAI/back/api/api_v1/endpoints/runner.py
@@ -1,4 +1,5 @@
 import logging
+from typing import Dict
 
 from fastapi import APIRouter, Depends, status
 from fastapi.exceptions import HTTPException
@@ -8,13 +9,14 @@ from sqlalchemy.orm import Session
 from DashAI.back.api.deps import get_db
 from DashAI.back.core.config import model_registry
 from DashAI.back.database.models import Dataset, Experiment, Run
-from DashAI.back.dataloaders.classes.dataset_dashai import DatasetDashAI
+from DashAI.back.dataloaders.classes.dashai_dataset import DashAIDataset, load
 
 logging.basicConfig(level=logging.DEBUG)
 log = logging.getLogger(__name__)
 
 router = APIRouter()
-metric_register = None #TBD
+metric_register = None  # TBD
+
 
 @router.post("/")
 async def execute_run(run_id: int, db: Session = Depends(get_db)):
@@ -23,9 +25,9 @@ async def execute_run(run_id: int, db: Session = Depends(get_db)):
 
     """
     try:
-        run : Run = db.get(Run, run_id)
-        exp : Experiment = db.get(Experiment, run.experiment_id)
-        dat : Dataset = db.get(Dataset, exp.dataset_id)
+        run: Run = db.get(Run, run_id)
+        exp: Experiment = db.get(Experiment, run.experiment_id)
+        dat: Dataset = db.get(Dataset, exp.dataset_id)
     except exc.SQLAlchemyError as e:
         log.exception(e)
         raise HTTPException(
@@ -34,26 +36,26 @@ async def execute_run(run_id: int, db: Session = Depends(get_db)):
         )
     run_instance = model_registry[run.model_name](run.parameters)
     dataset_instance = {
-        "train": DatasetDashAI.load_from_disk(f"{dat.file_path}/train"),
-        "test": DatasetDashAI.load_from_disk(f"{dat.file_path}/test"),
-        "validation": DatasetDashAI.load_from_disk(f"{dat.file_path}/validation"),
+        "train": load(f"{dat.file_path}/dataset/train"),
+        "test": load(f"{dat.file_path}/dataset/test"),
+        "validation": load(f"{dat.file_path}/dataset/validation"),
     }
     # Training
     run_instance.fit(dataset_instance["train"]["x"], dataset_instance["train"]["y"])
     # Evaluation
     metrics = metric_register.task_to_components(exp.task_name)
-    run.train_metrics = {
-        metric.name : metric.calc(
+    run.train_metrics: Dict[str, DashAIDataset] = {
+        metric.name: metric.calc(
             dataset_instance["train"]["y"],
-            run_instance.predict(dataset_instance["train"]["x"])
+            run_instance.predict(dataset_instance["train"]["x"]),
         )
         for metric in metrics
     }
     run.validation_metrics = {}
     run.test_metrics = {
-        metric.name : metric.calc(
+        metric.name: metric.calc(
             dataset_instance["test"]["y"],
-            run_instance.predict(dataset_instance["test"]["x"])
+            run_instance.predict(dataset_instance["test"]["x"]),
         )
         for metric in metrics
     }
