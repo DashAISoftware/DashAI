7fb1ea1b2ca49b0284f4185cdaa01cdf4cc21484
Oziel14 <ozielaguilera99@gmail.com>
2023-05-25 16:42:11 -0400

    Added the method to be able to format the data according to the model, made some adjustments to the predict function and some tests.

--- tests/back/models/test_tabular_class_models.py
+++ tests/back/models/test_tabular_class_models.py
@@ -3,81 +3,73 @@ from datasets import DatasetDict
 from sklearn.exceptions import NotFittedError
 from sklearn.utils.validation import check_is_fitted
 
-from DashAI.back.dataloaders.classes.dataset_dashai import DatasetDashAI
+from DashAI.back.dataloaders.classes.dashai_dataset import load_dataset
 from DashAI.back.models.scikit_learn.k_neighbors_classifier import KNeighborsClassifier
 from DashAI.back.models.scikit_learn.random_forest_classifier import (
     RandomForestClassifier,
 )
+from DashAI.back.models.scikit_learn.sklearn_like_model import SklearnLikeModel
 from DashAI.back.models.scikit_learn.svc import SVC
 
 
-@pytest.fixture(scope="module", name="load_datasetdashai")
-def fixture_load_datasetdashai():
-    paths = {
-        "path_train": "tests/back/models/datasetdashai_train",
-        "path_val": "tests/back/models/datasetdashai_validation",
-        "path_test": "tests/back/models/datasetdashai_test",
-    }
-    datasetdashai_train = DatasetDashAI.load_from_disk(paths["path_train"])
-    datasetdashai_val = DatasetDashAI.load_from_disk(paths["path_val"])
-    datasetdashai_test = DatasetDashAI.load_from_disk(paths["path_test"])
-    datasetdict_dashai = DatasetDict(
-        {
-            "train": datasetdashai_train,
-            "validation": datasetdashai_val,
-            "test": datasetdashai_test,
-        }
-    )
-    yield datasetdict_dashai
+@pytest.fixture(scope="module", name="load_dashaidataset")
+def fixture_load_dashaidataset():
+    dashai_datasetdict = load_dataset("tests/back/dataloaders/dashaidataset")
+    yield dashai_datasetdict
 
 
-def test_fit_models_tabular(load_datasetdashai: DatasetDict):
+def test_fit_models_tabular(load_dashaidataset: DatasetDict):
     knn = KNeighborsClassifier()
-    dataset_prepared = knn.format_data(load_datasetdashai)
-    knn.fit(dataset_prepared["x_train"], dataset_prepared["y_train"])
-    check_is_fitted(knn)
+    dataset_prepared = knn.format_data(load_dashaidataset)
+    knn.fit(dataset_prepared["train"]["input"], dataset_prepared["train"]["output"])
     rf = RandomForestClassifier()
-    dataset_prepared = rf.format_data(load_datasetdashai)
-    rf.fit(dataset_prepared["x_train"], dataset_prepared["y_train"])
-    check_is_fitted(rf)
+    dataset_prepared = rf.format_data(load_dashaidataset)
+    rf.fit(dataset_prepared["train"]["input"], dataset_prepared["train"]["output"])
     svm = SVC()
-    dataset_prepared = svm.format_data(load_datasetdashai)
-    svm.fit(dataset_prepared["x_train"], dataset_prepared["y_train"])
-    check_is_fitted(svm)
-    assert True
+    dataset_prepared = svm.format_data(load_dashaidataset)
+    svm.fit(dataset_prepared["train"]["input"], dataset_prepared["train"]["output"])
+    try:
+        check_is_fitted(knn)
+        check_is_fitted(rf)
+        check_is_fitted(svm)
+    except Exception as e:
+        pytest.fail(f"Unexpected error in test_fit_models_tabular: {repr(e)}")
 
 
-def test_predict_models_tabular(load_datasetdashai: DatasetDict):
+def test_predict_models_tabular(load_dashaidataset: DatasetDict):
     knn = KNeighborsClassifier()
-    dataset_prepared = knn.format_data(load_datasetdashai)
-    knn.fit(dataset_prepared["x_train"], dataset_prepared["y_train"])
-    knn.predict(dataset_prepared["x_test"])
+    dataset_prepared = knn.format_data(load_dashaidataset)
+    knn.fit(dataset_prepared["train"]["input"], dataset_prepared["train"]["output"])
+    pred_knn = knn.predict(dataset_prepared["test"]["input"])
 
     rf = RandomForestClassifier()
-    dataset_prepared = rf.format_data(load_datasetdashai)
-    rf.fit(dataset_prepared["x_train"], dataset_prepared["y_train"])
-    rf.predict(dataset_prepared["x_test"])
+    dataset_prepared = rf.format_data(load_dashaidataset)
+    rf.fit(dataset_prepared["train"]["input"], dataset_prepared["train"]["output"])
+    pred_ref = rf.predict(dataset_prepared["test"]["input"])
 
     svm = SVC()
-    dataset_prepared = svm.format_data(load_datasetdashai)
-    svm.fit(dataset_prepared["x_train"], dataset_prepared["y_train"])
-    svm.predict(dataset_prepared["x_test"])
-    assert True
+    dataset_prepared = svm.format_data(load_dashaidataset)
+    svm.fit(dataset_prepared["train"]["input"], dataset_prepared["train"]["output"])
+    pred_svm = svm.predict(dataset_prepared["test"]["input"])
+
+    assert len(dataset_prepared["test"]["input"]) == len(pred_knn)
+    assert len(dataset_prepared["test"]["input"]) == len(pred_ref)
+    assert len(dataset_prepared["test"]["input"]) == len(pred_svm)
 
 
-def test_not_fitted_models_tabular(load_datasetdashai: DatasetDict):
+def test_not_fitted_models_tabular(load_dashaidataset: DatasetDict):
+    rf = RandomForestClassifier()
+    dataset_prepared = rf.format_data(load_dashaidataset)
+
     with pytest.raises(NotFittedError):
-        knn = KNeighborsClassifier()
-        dataset_prepared = knn.format_data(load_datasetdashai)
-        knn.predict(dataset_prepared["x_test"])
-    assert True
+        rf.predict(dataset_prepared["test"]["input"])
 
 
-def test_save_and_load_model(load_datasetdashai: DatasetDict):
-    rf = RandomForestClassifier()
-    dataset_prepared = rf.format_data(load_datasetdashai)
-    rf.fit(dataset_prepared["x_train"], dataset_prepared["y_train"])
-    rf.save("tests/back/models/rfmodel")
-    model_rf = rf.load("tests/back/models/rfmodel")
-    model_rf.predict(dataset_prepared["x_test"])
-    assert True
+def test_save_and_load_model(load_dashaidataset: DatasetDict):
+    svm = SVC()
+    dataset_prepared = svm.format_data(load_dashaidataset)
+    svm.fit(dataset_prepared["train"]["input"], dataset_prepared["train"]["output"])
+    svm.save("tests/back/models/svm_model")
+    model_svm = SklearnLikeModel.load("tests/back/models/svm_model")
+    pred_svm = model_svm.predict(dataset_prepared["test"]["input"])
+    assert len(dataset_prepared["test"]["input"]) == len(pred_svm)
