52bc1bf9aa69d2242150be4f2216426909c68b18
Pablo Badilla <pbadilla.torrealba@gmail.com>
2023-05-26 00:11:32 -0400

    Finished base implementation of the new Registry.
    - get_child_classes and query_components_by_type methods re-implemented.
    - fixed some bugs in the previous implemented registry methods.
    - tested most of the registry methods.

--- DashAI/back/registries/base_registry_v2.py
+++ DashAI/back/registries/base_registry_v2.py
@@ -1,3 +1,5 @@
+from typing import Any
+
 from DashAI.back.registries.relationship_manager import RelationshipManager
 
 
@@ -62,6 +64,9 @@ class Registry:
         bool
             True if the component exists in the task registry, False otherwise.
         """
+        if not isinstance(item, str):
+            raise TypeError(f"The key should be str, got {item}.")
+
         for base_type_container in self._registry.values():
             if item in base_type_container:
                 return True
@@ -94,7 +99,7 @@ class Registry:
             if key in base_type_container:
                 return base_type_container[key]
 
-        raise KeyError(f"{key} does not exists in the registry.")
+        raise KeyError(f"Component '{key}' does not exists in the registry.")
 
     def _get_base_type(self, new_component: type) -> str:
         # select only base classes ancestors
@@ -104,27 +109,27 @@ class Registry:
             if "Base" in ancestor.__name__
         ]
 
-        type_cantidates = [
-            ancestor_cls.TYPE
+        base_classes_cantidates = [
+            ancestor_cls
             for ancestor_cls in component_base_ancestors
             if hasattr(ancestor_cls, "TYPE")
         ]
 
         # check if there is only one type candidate.
-        if len(type_cantidates) == 0:
+        if len(base_classes_cantidates) == 0:
             raise TypeError(
                 f"Component {new_component.__name__} does not a DashAI base class with "
                 f"a 'TYPE' class attribute. Classes that the component extends (MRO): "
                 f"{new_component.__mro__}"
             )
-        elif len(type_cantidates) > 1:
+        elif len(base_classes_cantidates) > 1:
             raise TypeError(
                 f"Component {new_component.__name__} has more than one base class with "
-                f"a 'TYPE' class attribute. Classes that the component extends (MRO): "
-                f"{new_component.__mro__}"
+                f"a 'TYPE' class attribute: "
+                f"{[_cls.__name__ for _cls in base_classes_cantidates]}."
             )
 
-        return type_cantidates[0]
+        return base_classes_cantidates[0].TYPE
 
     def register_component(self, new_component: type) -> None:
         """Register a component within the registry.
@@ -163,10 +168,12 @@ class Registry:
             )
 
         new_register_component = {
+            "name": new_component.__name__,
             "type": base_type,
             "class": new_component,
             "configurable_object": is_configurable_object,
             "schema": new_component.get_schema() if is_configurable_object else None,
+            "description": getattr(new_component, "DESCRIPTION", None),
         }
 
         if base_type not in self._registry:
@@ -180,3 +187,139 @@ class Registry:
                     new_component.__name__,
                     compatible_task,
                 )
+
+    def query_components_by_type(
+        self,
+        select: str | list[str] | None = None,
+        ignore: str | list[str] | None = None,
+    ) -> list[str, dict[str, Any]]:
+        """Obtains all the components according to the indicated types.
+
+        Only one of select or ignore should be provided.
+        In any other case an exception will be raised.
+
+        Parameters
+        ----------
+        select : str | list[str] | None, optional
+            The types of components selected for return, by default None
+        ignore : str | list[str] | None, optional
+            The types of components ignored for return., by default None
+
+        Returns
+        -------
+        list[str, dict[str, Any]]
+            A list with the selected components.
+
+        Raises
+        ------
+        ValueError
+            If select and ignore are not None.
+        ValueError
+            If select and ignore are None.
+        TypeError
+            When select provided, if select is not a string o a list of strings.
+        TypeError
+            When select provided, if some element of select list is not a string.
+        ValueError
+            When select provided, when a provided type does not exists in the registry.
+        TypeError
+            When ignore provided, if ignore is not a string o a list of strings.
+        TypeError
+            When ignore provided, if some element of ignore list is not a string.
+        ValueError
+            When ignore provided, when a provided type does not exists in the registry.
+        """
+        if select is not None and ignore is not None:
+            raise ValueError(
+                "Only select or ignore can be provided, not both at the same time."
+            )
+
+        if select is None and ignore is None:
+            raise ValueError(
+                "At least one of select or ignore parameters must be non-null."
+            )
+
+        # case 1: select is provided.
+        if select is not None:
+            # check passed select types
+            if not isinstance(select, (str, list)):
+                raise TypeError(
+                    f"Select must be a string or an array of strings, got {select}."
+                )
+            for idx, type_ in enumerate(select):
+                if not isinstance(type_, str):
+                    raise TypeError(
+                        f"type in position {idx} should be a string, got {type_}."
+                    )
+                if type_ not in self._registry:
+                    raise ValueError(
+                        f"Component type {type_} does not exists in the registry."
+                    )
+
+            # cast select into string
+            if isinstance(select, str):
+                select = [select]
+
+            return [
+                component
+                for selected_type in select
+                for component in self._registry[selected_type]
+            ]
+
+        # case 2: ignore is provided.
+        else:
+            # check passed ignore types
+            if not isinstance(select, (str, list)):
+                raise TypeError(
+                    f"Ignore must be a string or an array of strings, got {ignore}."
+                )
+            # cast select into string
+            if isinstance(select, str):
+                select = [select]
+
+            # check each type
+            for idx, type_ in enumerate(ignore):
+                if not isinstance(type_, str):
+                    raise TypeError(
+                        f"type in position {idx} should be a string, got {type_}."
+                    )
+                if type_ not in self._registry:
+                    raise ValueError(
+                        f"Component type {type_} does not exists in the registry."
+                    )
+
+            return [
+                component
+                for component_type in self.registry
+                if component_type not in ignore
+                for component in self._registry[component_type]
+            ]
+
+    def get_child_classes(self, parent_name: str) -> list[str]:
+        """Obtain the compoments that inherits from the specified parent component.
+
+        Note that the method will not raise an exception when a non existant parent
+        name is passed.
+
+        Parameters
+        ----------
+        parent_name : str
+            Class name of the parent component
+
+        Returns
+        -------
+        List[str]
+            List of component names that inherits from the parent component.
+        """
+        selected_components = []
+        for type_registry in self._registry.values():
+            for component_dict in type_registry.values():
+                component_bases = [
+                    base_class.__name__
+                    for base_class in component_dict["class"].__bases__
+                    if base_class.__name__ != "object"
+                ]
+                if parent_name in component_bases:
+                    selected_components.append(component_dict)
+
+        return [component["name"] for component in selected_components]
--- tests/back/registries/test_registry_v2.py
+++ tests/back/registries/test_registry_v2.py
@@ -3,11 +3,16 @@ import pytest
 from DashAI.back.config_object import ConfigObject
 from DashAI.back.registries.base_registry_v2 import Registry
 
-TEST_SCHEMA = {
+TEST_SCHEMA_1 = {
     "properties": {
         "parameter_1": {
             "type": "number",
         },
+    }
+}
+
+TEST_SCHEMA_2 = {
+    "properties": {
         "parameter_2": {
             "type": "string",
             "enum": ["a", "b"],
@@ -20,28 +25,44 @@ class BaseTask:
     TYPE = "Task"
 
 
-class BaseConfigurableComponent(ConfigObject):
-    TYPE = "GenericComponent"
+class BaseConfigurableComponent1(ConfigObject):
+    TYPE = "GenericComponent1"
+
+
+class BaseConfigurableComponent2(ConfigObject):
+    TYPE = "GenericComponent2"
 
 
 class BaseStaticComponent:
     TYPE = "StaticComponent"
 
 
-class ConfigurableComponent1(BaseConfigurableComponent):
+class ConfigurableComponent1(BaseConfigurableComponent1):
     @classmethod
     def get_schema(cls):
-        return TEST_SCHEMA
+        return TEST_SCHEMA_1
 
 
-class ConfigurableComponent2(BaseConfigurableComponent):
+class ConfigurableComponent2(BaseConfigurableComponent1):
     @classmethod
     def get_schema(cls):
-        return TEST_SCHEMA
+        return TEST_SCHEMA_2
+
+
+class SubConfigurableComponent1(ConfigurableComponent1):
+    @classmethod
+    def get_schema(cls):
+        return TEST_SCHEMA_1
+
+
+class ConfigurableComponentWithTwoBaseClasses(
+    BaseConfigurableComponent1, BaseConfigurableComponent2
+):
+    ...
 
 
 class StaticComponent1(BaseStaticComponent):
-    ...
+    DESCRIPTION = "Some static component"
 
 
 class NoComponent:
@@ -80,6 +101,38 @@ def test__init__bad_tasks_argument():
         Registry(initial_components=[1, 2, 3])
 
 
+def test_basic_register_component():
+    # this test does not include relationship creation.
+    test_registy = Registry()
+    test_registy.register_component(ConfigurableComponent1)
+
+    assert test_registy.registry == {
+        "GenericComponent1": {
+            "ConfigurableComponent1": {
+                "type": "GenericComponent1",
+                "class": ConfigurableComponent1,
+                "configurable_object": True,
+                "schema": TEST_SCHEMA_1,
+                "description": None,
+            }
+        }
+    }
+
+
+def test_basic_register_component_class_with_no_type():
+    # this test does not include relationship creation.
+    test_registy = Registry()
+
+    with pytest.raises(
+        TypeError,
+        match=(
+            r"Component NoComponent does not a DashAI base class with a 'TYPE' "
+            r"class attribute.*"
+        ),
+    ):
+        test_registy.register_component(NoComponent)
+
+
 def test__init__with_components():
     # init the test with two components
     test_registry = Registry(
@@ -94,24 +147,168 @@ def test__init__with_components():
     assert isinstance(test_registry.registry, dict)
 
     assert test_registry.registry == {
-        "GenericComponent": {
+        "GenericComponent1": {
             "ConfigurableComponent1": {
-                "type": "GenericComponent",
+                "type": "GenericComponent1",
                 "class": ConfigurableComponent1,
                 "configurable_object": True,
-                "schema": TEST_SCHEMA,
+                "schema": TEST_SCHEMA_1,
+                "description": None,
             },
             "ConfigurableComponent2": {
-                "type": "GenericComponent",
+                "type": "GenericComponent1",
                 "class": ConfigurableComponent2,
                 "configurable_object": True,
-                "schema": TEST_SCHEMA,
+                "schema": TEST_SCHEMA_2,
+                "description": None,
             },
+        },
+        "StaticComponent": {
             "StaticComponent1": {
                 "type": "StaticComponent",
                 "class": StaticComponent1,
                 "configurable_object": False,
                 "schema": None,
-            },
-        }
+                "description": "Some static component",
+            }
+        },
     }
+
+
+def test_get_base_type_two_base_classes_typerror():
+    test_register = Registry()
+    with pytest.raises(
+        TypeError,
+        match=(
+            r"Component ConfigurableComponentWithTwoBaseClasses has more than one base "
+            r"class with a 'TYPE' class attribute: "
+            r"\['ConfigurableComponentWithTwoBaseClasses', 'BaseConfigurableComponent1'"
+            r", 'BaseConfigurableComponent2'\]."
+        ),
+    ):
+        test_register._get_base_type(ConfigurableComponentWithTwoBaseClasses)
+
+
+def test_get_base_type_no_base_classes_typerror():
+    test_register = Registry()
+    with pytest.raises(
+        TypeError,
+        match=(
+            r"Component NoComponent does not a DashAI base class with a 'TYPE' class "
+            r"attribute.*"
+        ),
+    ):
+        test_register._get_base_type(NoComponent)
+
+
+def test_get_base_type():
+    test_register = Registry()
+    assert test_register._get_base_type(StaticComponent1) == "StaticComponent"
+    assert test_register._get_base_type(ConfigurableComponent1) == "GenericComponent1"
+    assert test_register._get_base_type(ConfigurableComponent2) == "GenericComponent1"
+
+
+def test__contains__():
+    test_registry = Registry(
+        initial_components=[
+            ConfigurableComponent1,
+            StaticComponent1,
+        ]
+    )
+
+    assert "ConfigurableComponent1" in test_registry
+    assert "StaticComponent1" in test_registry
+    assert "ConfigurableComponent2" not in test_registry
+
+
+def test__contains__key_type_error():
+    test_registry = Registry(
+        initial_components=[
+            ConfigurableComponent1,
+            StaticComponent1,
+        ]
+    )
+    with pytest.raises(TypeError, match=r"The key should be str, got \<.*\>."):
+        ConfigurableComponent1 in test_registry  # noqa
+
+
+def test__getitem__():
+    test_registry = Registry(
+        initial_components=[
+            ConfigurableComponent1,
+            ConfigurableComponent2,
+            StaticComponent1,
+        ]
+    )
+    assert test_registry["ConfigurableComponent1"] == {
+        "type": "GenericComponent1",
+        "class": ConfigurableComponent1,
+        "configurable_object": True,
+        "schema": TEST_SCHEMA_1,
+        "description": None,
+    }
+    assert test_registry["ConfigurableComponent2"] == {
+        "type": "GenericComponent1",
+        "class": ConfigurableComponent2,
+        "configurable_object": True,
+        "schema": TEST_SCHEMA_2,
+        "description": None,
+    }
+
+    assert test_registry["StaticComponent1"] == {
+        "type": "StaticComponent",
+        "class": StaticComponent1,
+        "configurable_object": False,
+        "schema": None,
+        "description": "Some static component",
+    }
+
+
+def test__getitem__key_type_error():
+    test_registry = Registry(
+        initial_components=[
+            ConfigurableComponent1,
+            ConfigurableComponent2,
+        ]
+    )
+
+    with pytest.raises(
+        TypeError,
+        match=r"The indexer should be a string, got \<class .*\>.",
+    ):
+        test_registry[StaticComponent1]
+
+
+def test__getitem__key_error():
+    test_registry = Registry(
+        initial_components=[
+            ConfigurableComponent1,
+            ConfigurableComponent2,
+        ]
+    )
+
+    with pytest.raises(
+        KeyError,
+        match=r"Component 'StaticComponent1' does not exists in the registry.",
+    ):
+        test_registry["StaticComponent1"]
+
+
+def test_get_child_classes():
+    test_registry = Registry(
+        initial_components=[
+            ConfigurableComponent1,
+            ConfigurableComponent2,
+            SubConfigurableComponent1,
+        ]
+    )
+    assert test_registry.get_child_classes("BaseConfigurableComponent1") == [
+        "ConfigurableComponent1",
+        "ConfigurableComponent2",
+    ]
+    assert test_registry.get_child_classes("ConfigurableComponent1") == [
+        "SubConfigurableComponent1"
+    ]
+    assert test_registry.get_child_classes("ConfigurableComponent2") == []
+
+    assert test_registry.get_child_classes("XYZ") == []
