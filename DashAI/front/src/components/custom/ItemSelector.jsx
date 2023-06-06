79429eea5e0b4fc7b7f0363e5ca53ae70bc02729
maximi4321 <maximiliano.aguilar@ug.uchile.cl>
2023-06-06 16:51:14 -0400

    fixes for task

--- DashAI/front/src/components/custom/ItemSelector.jsx
+++ DashAI/front/src/components/custom/ItemSelector.jsx
@@ -47,7 +47,7 @@ function ItemSelector({ itemsList, selectedItem, setSelectedItem }) {
   useEffect(() => {
     if (selectedItem !== undefined && Object.keys(selectedItem).length > 0) {
       const previouslySelectedItemIndex = itemsList.findIndex(
-        (item) => item.class === selectedItem.class,
+        (item) => item.name === selectedItem.name,
       );
       setSelectedIndex(previouslySelectedItemIndex);
     }
@@ -85,7 +85,7 @@ function ItemSelector({ itemsList, selectedItem, setSelectedItem }) {
           {itemsList.map((item, index) => {
             return (
               <ListItem
-                key={`list-button-${item.class}`}
+                key={`list-button-${item.name}`}
                 disablePadding
                 sx={{ display: itemsToShow[index] ? "show" : "none" }}
               >
@@ -106,11 +106,10 @@ function ItemSelector({ itemsList, selectedItem, setSelectedItem }) {
 }
 
 ItemSelector.propTypes = {
-  itemsList: PropTypes.arrayOf(PropTypes.shape({ class: PropTypes.string }))
+  itemsList: PropTypes.arrayOf(PropTypes.shape({ name: PropTypes.string }))
     .isRequired,
   selectedItem: PropTypes.shape({
     name: PropTypes.string,
-    class: PropTypes.string,
     description: PropTypes.string,
     images: PropTypes.arrayOf(PropTypes.string),
   }),
