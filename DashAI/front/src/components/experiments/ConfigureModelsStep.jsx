be99e4ce02aa7de3159f5478c534dbd7c9148f76
maximi4321 <maximiliano.aguilar@ug.uchile.cl>
2023-06-06 16:51:43 -0400

    fixes for model

--- DashAI/front/src/components/experiments/ConfigureModelsStep.jsx
+++ DashAI/front/src/components/experiments/ConfigureModelsStep.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import PropTypes from "prop-types";
 import { Button, Grid, MenuItem, TextField, Typography } from "@mui/material";
 import { AddCircleOutline as AddIcon } from "@mui/icons-material";
+// import { getComponents as getComponentsRequest } from "../../api/component";
 import { getModelSchema as getModelSchemaRequest } from "../../api/oldEndpoints";
 import { useSnackbar } from "notistack";
 import ModelsTable from "./ModelsTable";
@@ -14,25 +15,53 @@ import uuid from "react-uuid";
  * @param {function} setNewExp updates the Eperimento Modal state (newExp)
  * @param {function} setNextEnabled function to enable or disable the "Next" button in the modal
  */
+
+const compatibleModelsTest = [
+  { name: "KNeighborsClassifier" },
+  { name: "RandomForestClassifier" },
+  { name: "SVC" },
+  { name: "numericalWrapperForText" },
+  { name: "tcTransformerEngSpa" },
+];
+
 function ConfigureModelsStep({ newExp, setNewExp, setNextEnabled }) {
   const { enqueueSnackbar } = useSnackbar();
   const [modelNickname, setModelNickname] = useState("");
   const [selectedModel, setSelectedModel] = useState("");
-
-  const compatibleModels = [
-    "KNeighborsClassifier",
-    "RandomForestClassifier",
-    "SVC",
-    "numericalwrapperfortext",
-    "tcTransformerEngSpa",
-  ];
+  const [compatibleModels] = useState(compatibleModelsTest);
 
   // width for model nickname and model type textfields
   const textFieldWidth = "32vw";
 
+  // const getCompatibleModels = async () => {
+  //   try {
+  //     const models = await getComponentsRequest({
+  //       selectTypes: ["Model"],
+  //       relatedComponent: newExp.task_name,
+  //     });
+  //     setCompatibleModels(models);
+  //   } catch (error) {
+  //     enqueueSnackbar("Error while trying to obtain compatible models", {
+  //       variant: "error",
+  //       anchorOrigin: {
+  //         vertical: "top",
+  //         horizontal: "right",
+  //       },
+  //     });
+  //     if (error.response) {
+  //       console.error("Response error:", error.message);
+  //     } else if (error.request) {
+  //       console.error("Request error", error.request);
+  //     } else {
+  //       console.error("Unkown Error", error.message);
+  //     }
+  //   }
+  // };
+
   const getModelSchema = async () => {
     try {
       const schema = await getModelSchemaRequest(selectedModel);
+      console.log(selectedModel, schema);
       return schema;
     } catch (error) {
       enqueueSnackbar("Error while trying to obtain model schema", {
@@ -76,6 +105,11 @@ function ConfigureModelsStep({ newExp, setNewExp, setNextEnabled }) {
     }
   }, [newExp]);
 
+  // in mount, fetches the compatible models with the previously selected task
+  // useEffect(() => {
+  //   getCompatibleModels();
+  // }, []);
+
   return (
     <Grid
       container
@@ -113,8 +147,8 @@ function ConfigureModelsStep({ newExp, setNewExp, setNextEnabled }) {
               sx={{ width: textFieldWidth }}
             >
               {compatibleModels.map((model) => (
-                <MenuItem key={model} value={model}>
-                  {model}
+                <MenuItem key={model.name} value={model.name}>
+                  {model.name}
                 </MenuItem>
               ))}
             </TextField>
