cf0e855dcf72978457063706135b5f857403f2fc
maximi4321 <maximiliano.aguilar@ug.uchile.cl>
2023-06-12 16:50:17 -0400

    use component for itemsSelector

--- DashAI/front/src/components/ExperimentsDrawer.jsx
+++ DashAI/front/src/components/ExperimentsDrawer.jsx
@@ -1,21 +1,9 @@
 import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
-import {
-  Drawer,
-  IconButton,
-  Typography,
-  List,
-  ListItem,
-  TextField,
-  InputAdornment,
-  CircularProgress,
-  ListItemButton,
-  ListItemText,
-  Divider,
-} from "@mui/material";
-import ClearIcon from "@mui/icons-material/Clear";
+import { Drawer, Typography, Divider } from "@mui/material";
 import { getExperiments as getExperimentsRequest } from "../api/experiment";
 import { useSnackbar } from "notistack";
+import ItemSelector from "./custom/ItemSelector";
 
 const drawerWidth = "15vw";
 /**
@@ -26,38 +14,20 @@ function ExperimentsDrawer() {
   const navigate = useNavigate();
 
   const [experiments, setExperiments] = useState([]);
-  const [displayedExperiments, setDisplayedExperiments] = useState(
-    experiments.map(() => true),
-  );
-  const [searchField, setSearchField] = useState("");
+  const [selectedExperiment, setSelectedExperiment] = useState({});
   const [loading, setLoading] = useState(false);
-  const [selectedExperimentIndex, setSelectedExperimentIndex] = useState(null);
 
-  const handleExperimentsListItemClick = (event, index) => {
-    setSelectedExperimentIndex(index);
-    navigate(`/app/results/experiments/${experiments[index].id}`);
-  };
-
-  const handleClearSearchField = (event) => {
-    setSearchField("");
-    setDisplayedExperiments(experiments.map(() => true));
-  };
-
-  const handleSearchFieldChange = (event) => {
-    setSearchField(event.target.value);
-    setDisplayedExperiments(
-      experiments.map((val) =>
-        val.name.toLowerCase().includes(event.target.value.toLowerCase()),
-      ),
-    );
-  };
+  useEffect(() => {
+    if (selectedExperiment && "id" in selectedExperiment) {
+      navigate(`/app/results/experiments/${selectedExperiment.id}`);
+    }
+  }, [selectedExperiment]);
 
   const getExperiments = async () => {
     setLoading(true);
     try {
       const experiments = await getExperimentsRequest();
       setExperiments(experiments);
-      setDisplayedExperiments(experiments.map(() => true));
     } catch (error) {
       enqueueSnackbar("Error while trying to obtain the experiment table.", {
         variant: "error",
@@ -110,61 +80,13 @@ function ExperimentsDrawer() {
       <Divider />
 
       {/* Selector of experiments */}
-      <List sx={{ width: "100%" }} dense>
-        <ListItem disablePadding>
-          <TextField
-            id="experiment-search-input"
-            label="Search..."
-            type="search"
-            variant="standard"
-            value={searchField}
-            onChange={handleSearchFieldChange}
-            fullWidth
-            size="small"
-            sx={{ mb: 2 }}
-            InputProps={{
-              endAdornment: (
-                <InputAdornment position="end" onClick={handleClearSearchField}>
-                  <IconButton>
-                    <ClearIcon />
-                  </IconButton>
-                </InputAdornment>
-              ),
-            }}
-          />
-        </ListItem>
-
-        {/* Loading item (activated when useEffect is requesting the task list) */}
-        {loading && (
-          <ListItem sx={{ display: "flex", justifyContent: "center" }}>
-            <CircularProgress color="inherit" />
-          </ListItem>
-        )}
-        {/* Rendered experiments */}
-        {experiments.map((experiment, index) => {
-          return (
-            <div key={index}>
-              {displayedExperiments}
-              <ListItem
-                key={`experiment-list-button-${experiment.name}`}
-                disablePadding
-                sx={{
-                  display: displayedExperiments[index] ? "show" : "none",
-                }}
-              >
-                <ListItemButton
-                  selected={selectedExperimentIndex === index}
-                  onClick={(event) =>
-                    handleExperimentsListItemClick(event, index)
-                  }
-                >
-                  <ListItemText primary={experiment.name} />
-                </ListItemButton>
-              </ListItem>
-            </div>
-          );
-        })}
-      </List>
+      {!loading && (
+        <ItemSelector
+          itemsList={experiments}
+          selectedItem={selectedExperiment}
+          setSelectedItem={setSelectedExperiment}
+        />
+      )}
     </Drawer>
   );
 }
