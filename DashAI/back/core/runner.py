db0360a0041c654d5f513523ee4d677fd1534407
Lurrea <rodrigo.urrea@ug.uchile.cl>
2023-06-14 09:51:45 -0400

    add Exception handling inside background task

--- DashAI/back/core/runner.py
+++ DashAI/back/core/runner.py
@@ -1,3 +1,4 @@
+import logging
 from typing import Dict, List
 
 from sqlalchemy.orm import Session
@@ -9,6 +10,13 @@ from DashAI.back.metrics import BaseMetric
 from DashAI.back.models import BaseModel
 from DashAI.back.tasks import BaseTask
 
+logging.basicConfig(level=logging.DEBUG)
+log = logging.getLogger(__name__)
+
+
+class RunnerError(Exception):
+    """Exception raised when the runner proccess fails."""
+
 
 def execute_run(
     dataset: Dict[str, DashAIDataset],
@@ -18,34 +26,45 @@ def execute_run(
     run: Run,
     db: Session,
 ):
-    # Prepare dataset
-    prepared_dataset = task.prepare_for_task(dataset)
-    # Format dataset: TBD move this to dataloader
-    formated_dataset = model.format_data(prepared_dataset)
-    # Training
-    run.set_status_as_started()
-    db.commit()
-    model.fit(formated_dataset["train"]["input"], formated_dataset["train"]["output"])
-    run.set_status_as_finished()
-    db.commit()
-    # Evaluation
-    model_metrics = {
-        split: {
-            metric.__name__: metric.score(
-                formated_dataset[split]["output"],
-                model.predict(formated_dataset[split]["input"]),
-            )
-            for metric in metrics
+    try:
+        # Prepare dataset
+        prepared_dataset = task.prepare_for_task(dataset)
+        # Format dataset: TBD move this to dataloader
+        formated_dataset = model.format_data(prepared_dataset)
+        # Training
+        run.set_status_as_started()
+        db.commit()
+        model.fit(
+            formated_dataset["train"]["input"], formated_dataset["train"]["output"]
+        )
+        run.set_status_as_finished()
+        db.commit()
+        # Evaluation
+        model_metrics = {
+            split: {
+                metric.__name__: metric.score(
+                    formated_dataset[split]["output"],
+                    model.predict(formated_dataset[split]["input"]),
+                )
+                for metric in metrics
+            }
+            for split in ["train", "validation", "test"]
         }
-        for split in ["train", "validation", "test"]
-    }
-    # Save the changes in the run
-    run.train_metrics = model_metrics["train"]
-    run.validation_metrics = model_metrics["train"]
-    run.test_metrics = model_metrics["train"]
-    run_path = f"{settings.USER_RUN_PATH}/{run.id}"
-    model.save(run_path)
-    run.run_path = run_path
-    db.commit()
-    # Close DB connection
-    db.close()
+        # Save the changes in the run
+        run.train_metrics = model_metrics["train"]
+        run.validation_metrics = model_metrics["train"]
+        run.test_metrics = model_metrics["train"]
+        run_path = f"{settings.USER_RUN_PATH}/{run.id}"
+        model.save(run_path)
+        run.run_path = run_path
+        db.commit()
+    except Exception as e:
+        log.exception(e)
+        run.set_status_as_error()
+        db.commit()
+        raise RunnerError(
+            "Internal runner error",
+        ) from e
+    finally:
+        # Close DB connection
+        db.close()
