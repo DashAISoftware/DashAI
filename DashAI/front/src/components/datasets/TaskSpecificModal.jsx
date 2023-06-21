46499cbd1ff96c9ad2e078ceccf441a56528a1c4
Millaray Valdivia Saldias <mws.jbs@gmail.com>
2023-06-21 16:46:37 -0400

    fixing task specific flow

--- DashAI/front/src/components/datasets/TaskSpecificModal.jsx
+++ DashAI/front/src/components/datasets/TaskSpecificModal.jsx
@@ -1,37 +1,9 @@
-import React, { useState, useRef } from "react";
+import React from "react";
 import PropTypes from "prop-types";
-/* import {
-  Dialog,
-  DialogTitle,
-  DialogContent,
-  Stepper,
-  Step,
-  DialogActions,
-  ButtonGroup,
-  Button,
-  Grid,
-  Typography,
-  StepButton,
-} from "@mui/material"; */
 import SelectTaskStep from "./SelectTaskStep";
 import SelectDataloaderStep from "./SelectDataloaderStep";
 import ConfigureAndUploadDataset from "./ConfigureAndUploadDataset";
-/* import { useSnackbar } from "notistack";
-import { uploadDataset as uploadDatasetRequest } from "../../api/datasets"; */
 
-/* const steps = [
-  { name: "selectTask", label: "Select Task" },
-  { name: "selectDataloader", label: "Select a way to upload" },
-  { name: "uploadDataset", label: "Configure and upload your dataset" },
-];
-*/
-const defaultNewDataset = {
-  task_name: "",
-  dataloader: "",
-  file: null,
-  url: "",
-  params: {},
-}; 
 
 /**
  * This component renders a modal that takes the user through the process of uploading a new dataset.
@@ -39,84 +11,7 @@ const defaultNewDataset = {
  * @param {function} setOpen function to modify the value of open
  * @param {function} updateDatasets function to update the datasets table, it is used when the modal closes
  */
-function TaskSpecificModal({ open, setOpen, updateDatasets, activeStep, setNextEnabled }) {
-  const [newDataset, setNewDataset] = useState(defaultNewDataset);
-  const formSubmitRef = useRef(null);
-  // const { enqueueSnackbar } = useSnackbar();
-/* 
-  const handleSubmitNewDataset = async () => {
-    try {
-      const formData = new FormData();
-      const dataloaderName = newDataset.params.dataloader_params.name;
-
-      formData.append(
-        "params",
-        JSON.stringify({
-          ...newDataset.params,
-          dataset_name:
-            dataloaderName !== "" ? dataloaderName : newDataset.file.name,
-        }),
-      );
-      formData.append("url", ""); // TODO: url handling
-      formData.append("file", newDataset.file);
-      await uploadDatasetRequest(formData);
-      enqueueSnackbar("Dataset uploaded successfully", {
-        variant: "success",
-        anchorOrigin: {
-          vertical: "top",
-          horizontal: "right",
-        },
-      });
-    } catch (error) {
-      console.error(error);
-      enqueueSnackbar("Error when trying to upload the dataset.", {
-        variant: "error",
-        anchorOrigin: {
-          vertical: "top",
-          horizontal: "right",
-        },
-      });
-    }
-  };
-
-  const handleCloseDialog = () => {
-    setActiveStep(0);
-    setNewDataset(defaultNewDataset);
-    setNextEnabled(false);
-    setTimeout(() => updateDatasets());
-    setOpen(false);
-  };
-
-  const handleStepButton = (stepIndex) => () => {
-    setActiveStep(stepIndex);
-  };
-
-  const handleNextButton = () => {
-    if (activeStep < steps.length - 1) {
-      setActiveStep(activeStep + 1);
-      setNextEnabled(false);
-    } else {
-      // trigger dataloader form submit
-      formSubmitRef.current.handleSubmit();
-    }
-  };
-
-  const handleBackButton = () => {
-    if (activeStep === 0) {
-      handleCloseDialog();
-    } else {
-      setActiveStep(activeStep - 1);
-    }
-  };
-
-  // submits the new dataset when it has all necessary data
-  useEffect(() => {
-    if (newDataset.file !== null && Object.keys(newDataset.params).length > 0) {
-      handleSubmitNewDataset();
-      handleCloseDialog();
-    }
-  }, [newDataset]);
- */
+function TaskSpecificModal({ newDataset, setNewDataset, formSubmitRef, activeStep, setNextEnabled }) {
   return (
     <>
         {/* Step 1: select task */}
@@ -148,9 +43,9 @@ function TaskSpecificModal({ open, setOpen, updateDatasets, activeStep, setNextE
   );
 }
 TaskSpecificModal.propTypes = {
-  open: PropTypes.bool.isRequired,
-  setOpen: PropTypes.func.isRequired,
-  updateDatasets: PropTypes.func.isRequired,
+  newDataset: PropTypes.bool.isRequired,
+  setNewDataset: PropTypes.func.isRequired,
+  formSubmitRef: PropTypes.func.isRequired,
   activeStep: PropTypes.func.isRequired,
   setNextEnabled: PropTypes.func.isRequired,
 };
