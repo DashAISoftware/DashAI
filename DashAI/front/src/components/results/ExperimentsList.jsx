b5874364897382e19ac654378eec764558296e66
maximi4321 <maximiliano.aguilar@ug.uchile.cl>
2023-07-18 14:42:30 -0400

    fix: intially select an exp according to url

--- DashAI/front/src/components/results/ExperimentsList.jsx
+++ DashAI/front/src/components/results/ExperimentsList.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import { Typography, Divider } from "@mui/material";
 import { getExperiments as getExperimentsRequest } from "../../api/experiment";
 import { useSnackbar } from "notistack";
@@ -11,21 +11,34 @@ import ItemSelector from "../custom/ItemSelector";
 function ExperimentsList() {
   const { enqueueSnackbar } = useSnackbar();
   const navigate = useNavigate();
+  const { id } = useParams();
 
   const [experiments, setExperiments] = useState([]);
   const [selectedExperiment, setSelectedExperiment] = useState({});
   const [loading, setLoading] = useState(false);
 
+  // updates url when an experiment from the list is selected
   useEffect(() => {
-    if (selectedExperiment && "id" in selectedExperiment) {
+    if (
+      selectedExperiment &&
+      "id" in selectedExperiment &&
+      parseInt(id) !== selectedExperiment.id // navigates only if a new url is selected
+    ) {
       navigate(`/app/results/experiments/${selectedExperiment.id}`);
     }
   }, [selectedExperiment]);
+
   const getExperiments = async () => {
     setLoading(true);
     try {
       const experiments = await getExperimentsRequest();
       setExperiments(experiments);
+      // if there is an id in the url, then initially selects the experiment that corresponds to that id
+      if (id !== undefined) {
+        setSelectedExperiment(
+          experiments.find((exp) => exp.id === parseInt(id)) ?? {},
+        );
+      }
     } catch (error) {
       enqueueSnackbar("Error while trying to obtain the experiment table.");
       if (error.response) {
