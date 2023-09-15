c3dec6638b683f02f4f5ea8175ca315bc58d6430
Lucas-CE <100120556+Lucas-CE@users.noreply.github.com>
2023-09-15 09:31:32 -0300

    BaseProcessor extends ConfigObject and ABCMeta
    Co-authored-by: Pablo Badilla Torrealba <pbadilla.torrealba@gmail.com>

--- DashAI/back/preprocessor/base_preprocessor.py
+++ DashAI/back/preprocessor/base_preprocessor.py
@@ -2,7 +2,11 @@ from abc import abstractmethod
 from typing import Final
 
 
-class BasePreprocessor:
+from abc import ABCMeta, abstractmethod
+from DashAI.back.config_object import ConfigObject
+
+
+class BasePreprocessor(ConfigObject, metaclass=ABCMeta):
     """Base class for DashAI preprocessor."""
 
     TYPE: Final[str] = "Preprocessor"
