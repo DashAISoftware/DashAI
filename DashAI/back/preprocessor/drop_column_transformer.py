fee20692bac98b049a8a2e35797e025840fb3705
Lucas-CE <lucas.carrascoestay@gmail.com>
2023-09-20 11:50:27 -0300

    feat: :sparkles: In the drop column transformer the transform signature is fixed, and allows other input types

--- DashAI/back/preprocessor/drop_column_transformer.py
+++ DashAI/back/preprocessor/drop_column_transformer.py
@@ -5,7 +5,19 @@ from datasets import DatasetDict
 class DropColumnTransformer(BaseTransformer):
     """Transformer to drop columns from the dataset"""
 
-    def transform(self, dataset: DatasetDict, initial_index: int, final_index: int):
+    def __init__(self, columns: list[str] | str | tuple[int, int]):
+        """Constructor
+
+        Parameters
+        ----------
+        columns : list[str] | str | tuple[int, int]
+            Columns to be dropped. In case of the list or str, the columns are dropped
+            by name. In case of the tuple, the dropped columns will be those between
+            the two indexes.
+        """
+        self.columns = columns
+
+    def transform(self, dataset: DatasetDict):
         """Transform the dataset by removing columns.
 
         Parameters
@@ -13,19 +25,16 @@ class DropColumnTransformer(BaseTransformer):
         dataset : DatasetDict
             Dataset to be transformed
 
-        initial_index : int
-            Initial index of the columns to be removed
-
-        final_index : int
-            Final index of the columns to be removed
-
         Returns
         -------
         DatasetDict
             Dataset transformed
         """
         for split in dataset:
-            dataset[split] = dataset[split].remove_columns(
-                dataset[split].column_names[initial_index:final_index]
-            )
+            if isinstance(self.columns, list) or isinstance(self.columns, str):
+                dataset[split] = dataset[split].remove_columns(self.columns)
+            elif isinstance(self.columns, tuple):
+                dataset[split] = dataset[split].remove_columns(
+                    dataset[split].column_names[self.columns[0] : self.columns[1]]
+                )
         return dataset
