85631be80f6bc67c8e84dfc453c626c09c490846
Lucas-CE <lucas.carrascoestay@gmail.com>
2023-09-30 15:45:01 -0300

    test: :white_check_mark: Drop column transformer first test implemented

---
+++ tests/back/preprocessor/test_drop_column_transformers.py
@@ -0,0 +1,90 @@
+import io
+
+import pytest
+from datasets import DatasetDict
+from starlette.datastructures import UploadFile
+
+from DashAI.back.dataloaders.classes.csv_dataloader import CSVDataLoader
+from DashAI.back.dataloaders.classes.dashai_dataset import DashAIDataset
+from DashAI.back.dataloaders.classes.dataloader import to_dashai_dataset
+from DashAI.back.preprocessor.drop_column_by_name_transformer import (
+    DropColumnByNameTransformer,
+)
+
+
+@pytest.fixture(scope="module", name="iris_dataset")
+def prepare_iris_dataset():
+    test_dataset_path = "tests/back/preprocessor/iris.csv"
+    dataloader_test = CSVDataLoader()
+
+    with open(test_dataset_path, "r") as file:
+        csv_binary = io.BytesIO(bytes(file.read(), encoding="utf8"))
+        file = UploadFile(csv_binary)
+
+    datasetdict = dataloader_test.load_data(
+        filepath_or_buffer=file,
+        temp_path="tests/back/preprocessor",
+        params={"separator": ","},
+    )
+
+    inputs_columns = [
+        "SepalLengthCm",
+        "SepalWidthCm",
+        "PetalLengthCm",
+        "PetalWidthCm",
+    ]
+
+    datasetdict = to_dashai_dataset(
+        datasetdict,
+        inputs_columns,
+        outputs_columns=["Species"],
+    )
+
+    return datasetdict
+
+
+@pytest.fixture(scope="module", name="iris_dataset_petal_width_dropped")
+def prepare_iris_petal_width_dropped_dataset():
+    test_dataset_path = "tests/back/preprocessor/iris_petal_width_dropped.csv"
+    dataloader_test = CSVDataLoader()
+
+    with open(test_dataset_path, "r") as file:
+        csv_binary = io.BytesIO(bytes(file.read(), encoding="utf8"))
+        file = UploadFile(csv_binary)
+
+    datasetdict = dataloader_test.load_data(
+        filepath_or_buffer=file,
+        temp_path="tests/back/preprocessor",
+        params={"separator": ","},
+    )
+
+    inputs_columns = ["SepalLengthCm", "SepalWidthCm", "PetalLengthCm"]
+
+    datasetdict = to_dashai_dataset(
+        datasetdict,
+        inputs_columns,
+        outputs_columns=["Species"],
+    )
+
+    return datasetdict
+
+
+def test_remove_input_column_with_column_name(
+    iris_dataset: DatasetDict, iris_dataset_petal_width_dropped: DatasetDict
+):
+    dropper = DropColumnByNameTransformer("PetalWidthCm")
+    dataset_obtained = dropper.transform(iris_dataset)
+    assert set(dataset_obtained.keys()) == set(iris_dataset_petal_width_dropped.keys())
+    for split in dataset_obtained:
+        dataset_split: DashAIDataset = dataset_obtained[split]
+        iris_dataset_dropped_split: DashAIDataset = iris_dataset_petal_width_dropped[
+            split
+        ]
+        assert len(dataset_split) == len(iris_dataset_dropped_split)
+        for column_name in dataset_split.column_names:
+            assert dataset_split[column_name] == iris_dataset_dropped_split[column_name]
+        assert dataset_split.features == iris_dataset_dropped_split.features
+        assert dataset_split.inputs_columns == iris_dataset_dropped_split.inputs_columns
+        assert (
+            dataset_split.outputs_columns == iris_dataset_dropped_split.outputs_columns
+        )
