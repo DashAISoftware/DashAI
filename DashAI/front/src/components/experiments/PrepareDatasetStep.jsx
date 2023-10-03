fd6d139648be11a6fb94ba1872a81b2d7d484d54
Millaray Valdivia Saldias <mws.jbs@gmail.com>
2023-10-02 22:45:03 -0300

    Add parse to input and output column range and numbers

--- DashAI/front/src/components/experiments/PrepareDatasetStep.jsx
+++ DashAI/front/src/components/experiments/PrepareDatasetStep.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import PropTypes from "prop-types";
 
 import {
@@ -10,6 +10,69 @@ import {
   Radio,
 } from "@mui/material";
 function PrepareDatasetStep({ newExp, setNewExp, setNextEnabled }) {
+  // columns numbers state
+  const [inputColumns, setInputColumns] = useState([]);
+  const [outputColumns, setOutputColumns] = useState([]);
+
+  // handle columns numbers change state
+  const [parseInputColumnsError, setParseInputColumnsError] = useState(false);
+  const [parseOutputColumnsError, setParseOutputColumnsError] = useState(false);
+  const [parseInputColumnsErrorText, setParseInputColumnsErrorText] =
+    useState("");
+  const [parseOutputColumnsErrorText, setParseOutputColumnsErrorText] =
+    useState("");
+  const rangeRegex = /^(\d+)(-(\d+))*(,(\d+)(-(\d+))*)*$/;
+
+  const parseRangeToNumbers = (value) => {
+    const numbersArray = [];
+    if (!rangeRegex.test(value)) {
+      throw new Error(
+        "Range and numbers must match the format on the example above",
+      );
+    }
+    const ranges = value.split(",");
+    ranges.forEach((range) => {
+      const [min, max] = range.split("-");
+      if (!range.includes("-")) {
+        numbersArray.push(parseInt(range));
+      } else if (parseInt(min) > parseInt(max)) {
+        throw new Error(
+          "The second number of a range must be greater than the first",
+        );
+      } else {
+        for (let i = parseInt(min); i <= parseInt(max); i++) {
+          numbersArray.push(i);
+        }
+      }
+    });
+    return numbersArray;
+  };
+
+  const handleInputColumnsChange = (event) => {
+    const input = event.target.value.replace(/ /g, ""); // todo: dont accept spaces between numbers
+    try {
+      const columnNumbers = parseRangeToNumbers(input);
+      setParseInputColumnsError(false);
+      setInputColumns(columnNumbers);
+      console.log(inputColumns);
+    } catch (error) {
+      setParseInputColumnsErrorText(error.message);
+      setParseInputColumnsError(true);
+    }
+  };
+  const handleOutputColumnsChange = (event) => {
+    const input = event.target.value.replace(/ /g, "");
+    try {
+      const columnNumbers = parseRangeToNumbers(input);
+      setParseOutputColumnsError(false);
+      setOutputColumns(columnNumbers);
+      console.log(outputColumns);
+    } catch (error) {
+      setParseOutputColumnsErrorText(error.message);
+      setParseOutputColumnsError(true);
+    }
+  };
+
   return (
     <React.Fragment>
       <Grid container spacing={1}>
@@ -35,6 +98,9 @@ function PrepareDatasetStep({ newExp, setNewExp, setNextEnabled }) {
           label="Input"
           fullWidth
           autoComplete="off"
+          onChange={handleInputColumnsChange}
+          error={parseInputColumnsError}
+          helperText={parseInputColumnsError ? parseInputColumnsErrorText : ""}
           sx={{ mb: 2 }}
         />
         <TextField
@@ -43,6 +109,11 @@ function PrepareDatasetStep({ newExp, setNewExp, setNextEnabled }) {
           label="Output"
           fullWidth
           autoComplete="off"
+          onChange={handleOutputColumnsChange}
+          error={parseOutputColumnsError}
+          helperText={
+            parseOutputColumnsError ? parseOutputColumnsErrorText : ""
+          }
           sx={{ mb: 2 }}
         />
         <Typography variant="subtitle1" component="h3" sx={{ mb: 2 }}>
