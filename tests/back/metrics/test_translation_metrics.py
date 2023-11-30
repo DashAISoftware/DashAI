954a6362e34ba51945e810671dbb8402bf5df54a
Lurrea <rodrigo.urrea@ug.uchile.cl>
2023-11-30 16:57:47 -0300

    fix translation test to use select_column method from DashAIDataset and generate a prediction using the Opus transformer

--- tests/back/metrics/test_translation_metrics.py
+++ tests/back/metrics/test_translation_metrics.py
@@ -1,15 +1,20 @@
 """Translation Metrics Tests."""
 import io
+from typing import Dict, Tuple
 
+import numpy as np
 import pytest
 from datasets import DatasetDict
 from starlette.datastructures import UploadFile
 
-from DashAI.back.dataloaders.classes.dashai_dataset import select_columns
+from DashAI.back.dataloaders.classes.dashai_dataset import DashAIDataset, select_columns
 from DashAI.back.dataloaders.classes.dataloader import to_dashai_dataset
 from DashAI.back.dataloaders.classes.json_dataloader import JSONDataLoader
 from DashAI.back.metrics.translation.bleu import Bleu
 from DashAI.back.metrics.translation.ter import Ter
+from DashAI.back.models.hugging_face.opus_mt_en_es_transformer import (
+    OpusMtEnESTransformer,
+)
 from DashAI.back.tasks.translation_task import TranslationTask
 
 
@@ -44,23 +49,29 @@ def dataset_fixture():
         dashai_dataset, 0.7, 0.1, 0.2, seed=42
     )
 
-    divided_dataset = select_columns(split_dataset, [*inputs_columns, *outputs_columns])
+    divided_dataset = select_columns(split_dataset, inputs_columns, outputs_columns)
 
     return divided_dataset
 
 
-def test_bleu(dataset: DatasetDict):
-    pred_opus_mt_en_es = dataset["test"][1]
-    score = Bleu.score(dataset["test"][1], pred_opus_mt_en_es)
+@pytest.fixture(scope="module", name="translation_pred")
+def fixture_translation_pred(dataset: Dict[str, Tuple[DashAIDataset, DashAIDataset]]):
+    params = {"num_train_epochs": 1, "batch_size": 32, "device": "cpu"}
+    model = OpusMtEnESTransformer(**params)
+    model.fit(dataset["train"][0], dataset["train"][1])
+    return model.predict(dataset["test"][1])
+
+
+def test_bleu(dataset: DatasetDict, translation_pred: np.ndarray):
+    score = Bleu.score(dataset["test"][1], translation_pred)
 
     assert isinstance(score, float)
     assert score >= 0.0
     assert score <= 1.0
 
 
-def test_ter(dataset: DatasetDict):
-    pred_opus_mt_en_es = dataset["test"][1]
-    score = Ter.score(dataset["test"][1], pred_opus_mt_en_es)
+def test_ter(dataset: DatasetDict, translation_pred: np.ndarray):
+    score = Ter.score(dataset["test"][1], translation_pred)
 
     assert isinstance(score, float)
     assert score >= 0.0
@@ -68,7 +79,7 @@ def test_ter(dataset: DatasetDict):
 
 
 def test_metrics_different_input_sizes(dataset: DatasetDict):
-    pred_opus_mt_en_es = dataset["test"][1]
+    pred_opus_mt_en_es = dataset["test"][1]["class"]
     with pytest.raises(
         ValueError, match="The length of the true and predicted labels must be equal."
     ):
