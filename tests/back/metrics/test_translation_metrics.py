13e169605cba03b1bea77a077afc23b88550f70e
Lurrea <rodrigo.urrea@ug.uchile.cl>
2023-11-30 11:43:56 -0300

    fix translation metric test

--- tests/back/metrics/test_translation_metrics.py
+++ tests/back/metrics/test_translation_metrics.py
@@ -5,6 +5,7 @@ import pytest
 from datasets import DatasetDict
 from starlette.datastructures import UploadFile
 
+from DashAI.back.dataloaders.classes.dashai_dataset import divide_by_columns
 from DashAI.back.dataloaders.classes.dataloader import to_dashai_dataset
 from DashAI.back.dataloaders.classes.json_dataloader import JSONDataLoader
 from DashAI.back.metrics.translation.bleu import Bleu
@@ -12,8 +13,8 @@ from DashAI.back.metrics.translation.ter import Ter
 from DashAI.back.tasks.translation_task import TranslationTask
 
 
-@pytest.fixture(scope="module", name="test_dataset")
-def translation_metrics_tests_fixture():
+@pytest.fixture(scope="module", name="dataset")
+def dataset_fixture():
     test_dataset_path = "tests/back/metrics/translationEngSpaDatasetSmall.json"
     json_dataloader = JSONDataLoader()
 
@@ -28,48 +29,56 @@ def translation_metrics_tests_fixture():
         params={"data_key": "data"},
     )
 
-    dashai_dataset = to_dashai_dataset(
-        dataset_dict,
-        inputs_columns=["text"],
-        outputs_columns=["class"],
-    )
+    dashai_dataset = to_dashai_dataset(dataset_dict)
+
+    inputs_columns = ["text"]
+    outputs_columns = ["class"]
 
     translation_task = TranslationTask()
-    dashai_dataset = translation_task.prepare_for_task(dashai_dataset)
-    translation_task.validate_dataset_for_task(dashai_dataset, "EngSpaDataset")
+    dashai_dataset = translation_task.prepare_for_task(dashai_dataset, outputs_columns)
+    translation_task.validate_dataset_for_task(
+        dashai_dataset, "EngSpaDataset", inputs_columns, outputs_columns
+    )
 
     split_dataset = json_dataloader.split_dataset(
         dashai_dataset, 0.7, 0.1, 0.2, seed=42
     )
-    return split_dataset
+
+    divided_dataset = divide_by_columns(
+        split_dataset,
+        inputs_columns,
+        outputs_columns,
+    )
+
+    return divided_dataset
 
 
-def test_bleu(test_dataset: DatasetDict):
-    pred_opus_mt_en_es = test_dataset["test"]["class"]
-    score = Bleu.score(test_dataset["test"], pred_opus_mt_en_es)
-
-    assert isinstance(score, float)
-    assert score >= 0.0
-    assert score <= 1.0
-
-
-def test_ter(test_dataset: DatasetDict):
-    pred_opus_mt_en_es = test_dataset["test"]["class"]
-    score = Ter.score(test_dataset["test"], pred_opus_mt_en_es)
+def test_bleu(dataset: DatasetDict):
+    pred_opus_mt_en_es = dataset["test"][1]
+    score = Bleu.score(dataset["test"][1], pred_opus_mt_en_es)
 
     assert isinstance(score, float)
     assert score >= 0.0
     assert score <= 1.0
 
 
-def test_metrics_different_input_sizes(test_dataset: DatasetDict):
-    pred_opus_mt_en_es = test_dataset["test"]["class"]
+def test_ter(dataset: DatasetDict):
+    pred_opus_mt_en_es = dataset["test"][1]
+    score = Ter.score(dataset["test"][1], pred_opus_mt_en_es)
+
+    assert isinstance(score, float)
+    assert score >= 0.0
+    assert score <= 1.0
+
+
+def test_metrics_different_input_sizes(dataset: DatasetDict):
+    pred_opus_mt_en_es = dataset["test"][1]
     with pytest.raises(
         ValueError, match="The length of the true and predicted labels must be equal."
     ):
-        Bleu.score(test_dataset["validation"], pred_opus_mt_en_es)
+        Bleu.score(dataset["validation"][1], pred_opus_mt_en_es)
 
     with pytest.raises(
         ValueError, match="The length of the true and predicted labels must be equal."
     ):
-        Ter.score(test_dataset["validation"], pred_opus_mt_en_es)
+        Ter.score(dataset["validation"][1], pred_opus_mt_en_es)
