3092c9aaf9f7eca60b98ffc2885d9c027c8f444a
Pablo Badilla <vn55on0>
2023-12-24 19:38:23 -0300

    Add CLI option to configure the package loggers

--- DashAI/__main__.py
+++ DashAI/__main__.py
@@ -1,11 +1,27 @@
+"""Main module for DashAI package.
+
+Contains the main function that is executed when the package is called from the
+command line.
+"""
+import logging
 import pathlib
 import webbrowser
+from enum import Enum
 
 import typer
 import uvicorn
 from typing_extensions import Annotated
 
 from DashAI.back.app import create_app
+
+
+class LoggingLevel(str, Enum):
+    NOTSET = "NOTSET"
+    DEBUG = "DEBUG"
+    INFO = "INFO"
+    WARN = "WARN"
+    ERROR = "ERROR"
+    CRITICAL = "CRITICAL"
 
 
 def open_browser():
@@ -17,9 +33,12 @@ def main(
     local_path: Annotated[
         pathlib.Path, typer.Option(help="Path where DashAI files will be stored.")
     ] = "~/.DashAI",
+    logging_level: Annotated[
+        LoggingLevel, typer.Option(help="Logging level.")
+    ] = LoggingLevel.INFO,
 ):
     uvicorn.run(
-        create_app(local_path=local_path),
+        create_app(local_path=local_path, logging_level=logging_level),
         host="127.0.0.1",
         port=8000,
     )
--- DashAI/back/app.py
+++ DashAI/back/app.py
@@ -1,8 +1,9 @@
 """FastAPI Application module."""
 import logging
 import pathlib
-from typing import Any, Dict, Union
+from typing import Any, Dict, Literal, Union
 
+from beartype import beartype
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
@@ -13,21 +14,22 @@ from DashAI.back.config import DefaultSettings
 from DashAI.back.containers import Container
 
 logger = logging.getLogger(__name__)
-logger.setLevel(logging.INFO)
 
 
 def _create_path_if_not_exists(new_path: str) -> None:
     """Create a new path if it does not exist."""
     if not new_path.exists():
-        logging.info("Creating new path: %s.", str(new_path))
+        logging.debug("Creating new path: %s.", str(new_path))
         new_path.mkdir(parents=True)
 
     else:
-        logger.info("Using existant path: %s.", str(new_path))
+        logger.debug("Using existant path: %s.", str(new_path))
 
 
+@beartype
 def _generate_config_dict(
-    local_path: Union[pathlib.Path, None] = None
+    local_path: Union[pathlib.Path, None],
+    logging_level: Literal["NOTSET", "DEBUG", "INFO", "WARN", "ERROR", "CRITICAL"],
 ) -> Dict[str, Any]:
     """Generate the initial app configuration.
 
@@ -40,6 +42,8 @@ def _generate_config_dict(
     local_path : Union[pathlib.Path, None], optional
         Path where DashAI files will be stored. If None, the default
         value of config (~/.DashAI) will be used , by default None.
+    logging_level: Literal["NOTSET", "DEBUG", "INFO", "WARN", "ERROR", "CRITICAL"],
+        Global app logging level, by default "INFO".
 
     Returns
     -------
@@ -61,21 +65,61 @@ def _generate_config_dict(
     settings["DATASETS_PATH"] = local_path / settings["DATASETS_PATH"]
     settings["RUNS_PATH"] = local_path / settings["RUNS_PATH"]
     settings["FRONT_BUILD_PATH"] = pathlib.Path(settings["FRONT_BUILD_PATH"]).absolute()
+    settings["LOGGING_LEVEL"] = getattr(logging, logging_level)
 
     return settings
 
 
-def create_app(local_path: Union[pathlib.Path, None] = None) -> FastAPI:
+def create_app(
+    local_path: Union[pathlib.Path, None] = None,
+    logging_level: Literal[
+        "NOTSET", "DEBUG", "INFO", "WARN", "ERROR", "CRITICAL"
+    ] = "INFO",
+) -> FastAPI:
+    """Create the main application.
+
+    Steps:
+    1. Initialize the dependency injection container and wires the subpackages.
+    2. Create the configuration dictionary and sets it as container configuration.
+    3. Set the logging level for all subpackages.
+    4. Create the local paths where the files are stored.
+    5. Initialize the SQlite database.
+    6. Initialize the FastAPI application and mount the API routers.
+
+    Parameters
+    ----------
+    local_path : Union[pathlib.Path, None], optional
+        Path where DashAI files will be stored , by default None
+    logging_level : Literal['NOTSET', 'DEBUG', 'INFO', 'WARN', 'ERROR', 'CRITICAL']
+        Set the package logging level. It affects all subpackages loggers that does
+        not specifies mannualy the logging level, by default "INFO"
+
+    Returns
+    -------
+    FastAPI
+        The created FastAPI application.
+    """
+
+    # initialize container and configurations
     container = Container()
-    config = _generate_config_dict(local_path=local_path)
+    config = _generate_config_dict(local_path=local_path, logging_level=logging_level)
     container.config.from_dict(config)
 
+    # set package and subpackages logging level
+    logging.getLogger(__package__).setLevel(
+        container.config.provided()["LOGGING_LEVEL"]
+    )
+
+    # create local paths
+    _create_path_if_not_exists(container.config.provided()["LOCAL_PATH"])
     _create_path_if_not_exists(container.config.provided()["DATASETS_PATH"])
     _create_path_if_not_exists(container.config.provided()["RUNS_PATH"])
 
+    # create database
     db = container.db()
     db.create_database()
 
+    # initialize app
     app = FastAPI(title="DashAI")
     api_v0 = FastAPI(title="DashAI API v0")
     api_v1 = FastAPI(title="DashAI API v1")
