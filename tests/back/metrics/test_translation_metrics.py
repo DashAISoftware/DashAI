1b32870768f813dfe023fb0969c40b7b465e04ea
Lurrea <rodrigo.urrea@ug.uchile.cl>
2024-01-04 22:30:59 -0300

    fix translation metric test

--- tests/back/metrics/test_translation_metrics.py
+++ tests/back/metrics/test_translation_metrics.py
@@ -46,7 +46,7 @@ def dataset_fixture():
     )
 
     split_dataset = json_dataloader.split_dataset(
-        dashai_dataset, 0.7, 0.1, 0.2, seed=42
+        dashai_dataset, 0.6, 0.2, 0.2, seed=42
     )
 
     divided_dataset = select_columns(split_dataset, inputs_columns, outputs_columns)
@@ -56,8 +56,7 @@ def dataset_fixture():
 
 @pytest.fixture(scope="module", name="translation_pred")
 def fixture_translation_pred(dataset: Tuple[DatasetDict, DatasetDict]):
-    params = {"num_train_epochs": 1, "batch_size": 32, "device": "cpu"}
-    model = OpusMtEnESTransformer(**params)
+    model = OpusMtEnESTransformer()
     model.fit(dataset[0]["train"], dataset[1]["train"])
     return model.predict(dataset[1]["test"])
 
@@ -83,9 +82,9 @@ def test_metrics_different_input_sizes(dataset: Tuple[DatasetDict, DatasetDict])
     with pytest.raises(
         ValueError, match="The length of the true and predicted labels must be equal."
     ):
-        Bleu.score(dataset[1]["validation"], pred_opus_mt_en_es)
+        Bleu.score(dataset[1]["train"], pred_opus_mt_en_es)
 
     with pytest.raises(
         ValueError, match="The length of the true and predicted labels must be equal."
     ):
-        Ter.score(dataset[1]["validation"], pred_opus_mt_en_es)
+        Ter.score(dataset[1]["train"], pred_opus_mt_en_es)
