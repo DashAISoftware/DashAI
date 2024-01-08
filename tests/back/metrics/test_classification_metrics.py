331bcbccdc95bd8d9bfb0c575981973c29c535a8
Lurrea <rodrigo.urrea@ug.uchile.cl>
2024-01-08 14:17:51 -0300

    refactor classification metric test to use pre-computed inputs instead to calculate them every time

--- tests/back/metrics/test_classification_metrics.py
+++ tests/back/metrics/test_classification_metrics.py
@@ -1,161 +1,82 @@
-import io
-import os
-from typing import Tuple
+from typing import Dict, List
 
+import numpy as np
 import pytest
-from datasets import DatasetDict
-from starlette.datastructures import UploadFile
+from datasets import Dataset
 
-from DashAI.back.dataloaders.classes.csv_dataloader import CSVDataLoader
-from DashAI.back.dataloaders.classes.dashai_dataset import select_columns
-from DashAI.back.dataloaders.classes.dataloader import to_dashai_dataset
 from DashAI.back.metrics.classification.accuracy import Accuracy
 from DashAI.back.metrics.classification.f1 import F1
 from DashAI.back.metrics.classification.precision import Precision
 from DashAI.back.metrics.classification.recall import Recall
-from DashAI.back.models.scikit_learn.random_forest_classifier import (
-    RandomForestClassifier,
-)
-from DashAI.back.tasks.tabular_classification_task import TabularClassificationTask
 
 
-@pytest.fixture(scope="module", name="dataset_and_model")
-def dataset_and_model_fixture() -> Tuple[DatasetDict, RandomForestClassifier]:
-    test_dataset_path = "tests/back/metrics/iris.csv"
-
-    csv_dataloader = CSVDataLoader()
-    with open(test_dataset_path, "r") as file:
-        csv_data = file.read()
-        csv_binary = io.BytesIO(bytes(csv_data, encoding="utf8"))
-        file = UploadFile(csv_binary)
-
-    dataset_dict = csv_dataloader.load_data(
-        filepath_or_buffer=file,
-        temp_path="tests/back/metrics",
-        params={"separator": ","},
-    )
-    dashai_dataset = to_dashai_dataset(dataset_dict)
-
-    dataset = csv_dataloader.split_dataset(
-        dataset=dashai_dataset,
-        train_size=0.7,
-        test_size=0.1,
-        val_size=0.2,
-    )
-
-    input_columns = ["SepalLengthCm", "SepalWidthCm", "PetalLengthCm", "PetalWidthCm"]
-    output_columns = ["Species"]
-
-    tabular_task = TabularClassificationTask()
-
-    name_datasetdict = "Iris"
-    dataset = tabular_task.prepare_for_task(dataset, output_columns)
-    tabular_task.validate_dataset_for_task(
-        dataset, name_datasetdict, input_columns, output_columns
-    )
-
-    divided_dataset = select_columns(
-        dataset,
-        input_columns,
-        output_columns,
-    )
-
-    model = RandomForestClassifier()
-    model.fit(divided_dataset[0]["train"], divided_dataset[1]["train"])
-    model.save("tests/back/metrics/rf_model")
-
-    yield divided_dataset, model
-
-    os.remove("tests/back/metrics/rf_model")
+@pytest.fixture(scope="module", name="metric_input")
+def fixture_metric_input() -> dict:
+    return {
+        "true_labels": Dataset.from_list([{"foo": 1}, {"foo": 2}, {"foo": 0}]),
+        "pred_labels": np.array([[0.1, 0.1, 0.8], [0.0, 0.8, 0.2], [0.9, 0.0, 0.5]]),
+        "wrong_size_labels": np.array([[0.1, 0.1, 0.8], [0.9, 0.0, 0.5]]),
+    }
 
 
-def test_accuracy(
-    dataset_and_model: Tuple[Tuple[DatasetDict, DatasetDict], RandomForestClassifier]
-):
-    dataset, model = dataset_and_model
-    y_pred = model.predict(dataset[0]["test"])
-    score = Accuracy.score(dataset[1]["test"], y_pred)
+def test_accuracy(metric_input: Dict[str, List[int]]):
+    score = Accuracy.score(metric_input["true_labels"], metric_input["pred_labels"])
 
     assert isinstance(score, float)
     assert score >= 0.0
     assert score <= 1.0
 
 
-def test_precision(
-    dataset_and_model: Tuple[Tuple[DatasetDict, DatasetDict], RandomForestClassifier]
-):
-    dataset, model = dataset_and_model
-    y_pred = model.predict(dataset[0]["test"])
-
-    score = Precision.score(dataset[1]["test"], y_pred)
+def test_precision(metric_input: Dict[str, List[int]]):
+    score = Precision.score(metric_input["true_labels"], metric_input["pred_labels"])
 
     assert isinstance(score, float)
     assert score >= 0.0
     assert score <= 1.0
 
 
-def test_recall(
-    dataset_and_model: Tuple[Tuple[DatasetDict, DatasetDict], RandomForestClassifier]
-):
-    dataset, model = dataset_and_model
-    y_pred = model.predict(dataset[0]["test"])
-    score = Recall.score(dataset[1]["test"], y_pred)
+def test_recall(metric_input: Dict[str, List[int]]):
+    score = Recall.score(metric_input["true_labels"], metric_input["pred_labels"])
 
     assert isinstance(score, float)
     assert score >= 0.0
     assert score <= 1.0
 
 
-def test_f1_score(
-    dataset_and_model: Tuple[Tuple[DatasetDict, DatasetDict], RandomForestClassifier]
-):
-    dataset, model = dataset_and_model
-    y_pred = model.predict(dataset[0]["test"])
-    score = F1.score(dataset[1]["test"], y_pred)
+def test_f1_score(metric_input: Dict[str, List[int]]):
+    score = F1.score(metric_input["true_labels"], metric_input["pred_labels"])
 
     assert isinstance(score, float)
     assert score >= 0.0
     assert score <= 1.0
 
 
-def test_metrics_different_input_sizes(
-    dataset_and_model: Tuple[Tuple[DatasetDict, DatasetDict], RandomForestClassifier]
-):
-    dataset, model = dataset_and_model
-    y_pred = model.predict(dataset[0]["validation"])
+def test_metrics_different_input_sizes(metric_input: Dict[str, List[int]]):
+    error_pattern = (
+        r"The length of the true labels and the predicted labels must be equal, "
+        r"given: len\(true_labels\) = 3 and len\(pred_labels\) = 2\."
+    )
 
     with pytest.raises(
         ValueError,
-        match=(
-            r"The length of the true labels and the predicted labels must be equal, "
-            r"given: len\(true_labels\) = 15 and len\(pred_labels\) = 30\."
-        ),
+        match=error_pattern,
     ):
-        Accuracy.score(dataset[1]["test"], y_pred)
+        Accuracy.score(metric_input["true_labels"], metric_input["wrong_size_labels"])
 
     with pytest.raises(
         ValueError,
-        match=(
-            r"The length of the true labels and the predicted labels must be equal, "
-            r"given: len\(true_labels\) = 15 and len\(pred_labels\) = 30\."
-        ),
+        match=error_pattern,
     ):
-        Precision.score(dataset[1]["test"], y_pred)
+        Precision.score(metric_input["true_labels"], metric_input["wrong_size_labels"])
 
     with pytest.raises(
         ValueError,
-        match=(
-            r"The length of the true labels and the predicted labels must be equal, "
-            r"given: len\(true_labels\) = 15 and len\(pred_labels\) = 30\."
-        ),
+        match=error_pattern,
     ):
-        Recall.score(dataset[1]["test"], y_pred)
+        Recall.score(metric_input["true_labels"], metric_input["wrong_size_labels"])
 
     with pytest.raises(
         ValueError,
-        match=(
-            r"The length of the true labels and the predicted labels must be equal, "
-            r"given: len\(true_labels\) = 15 and len\(pred_labels\) = 30\."
-        ),
+        match=error_pattern,
     ):
-        F1.score(dataset[1]["test"], y_pred)
+        F1.score(metric_input["true_labels"], metric_input["wrong_size_labels"])
