555bbb3cc325d89bdefdf41f1d0ef544b87be03b
Lurrea <rodrigo.urrea@ug.uchile.cl>
2024-01-23 11:18:18 -0300

    added schema base class

---
+++ DashAI/back/core/schema_fields/__init__.py
@@ -0,0 +1,5 @@
+# flake8: noqa
+from DashAI.back.core.schema_fields.base_schema import BaseSchema
+from DashAI.back.core.schema_fields.int_field import int_field
+from DashAI.back.core.schema_fields.object_field import component_field
+from DashAI.back.core.schema_fields.string_field import string_field
