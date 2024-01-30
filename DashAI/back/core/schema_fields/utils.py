39fa4cb56f565f513144743bb0a45a6a44dac2ec
Lurrea <rodrigo.urrea@ug.uchile.cl>
2024-01-30 14:28:54 -0300

    fix registry usage in fill_objects

--- DashAI/back/core/schema_fields/utils.py
+++ DashAI/back/core/schema_fields/utils.py
@@ -1,9 +1,10 @@
-from dependency_injector.wiring import Provide
+from dependency_injector.wiring import Provide, inject
 
 from DashAI.back.core.schema_fields.base_schema import BaseSchema
 from DashAI.back.core.schema_fields.object_field import ComponentType
 
 
+@inject
 def fill_objects(
     schema_instance: BaseSchema,
     component_registry=Provide["component_registry"],
@@ -31,8 +32,12 @@ def fill_objects(
     """
     for field_name, field_value in iter(schema_instance):
         if isinstance(field_value, ComponentType):
-            component_class = component_registry[field_value.component]
+            component_class = component_registry[field_value.component]["class"]
             validated_params = component_class.SCHEMA.model_validate(field_value.params)
             complete_params = fill_objects(validated_params)
-            setattr(schema_instance, field_name, component_class(complete_params))
+            setattr(
+                schema_instance,
+                field_name,
+                component_class(**complete_params.model_dump()),
+            )
     return schema_instance
