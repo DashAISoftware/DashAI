3b3dd471b65257c31629d6a8dabe6ecd99ac7186
Lurrea <rodrigo.urrea@ug.uchile.cl>
2024-01-31 12:31:12 -0300

    rename file in init

--- DashAI/back/core/schema_fields/__init__.py
+++ DashAI/back/core/schema_fields/__init__.py
@@ -1,6 +1,6 @@
 # flake8: noqa
 from DashAI.back.core.schema_fields.base_schema import BaseSchema
 from DashAI.back.core.schema_fields.int_field import int_field
-from DashAI.back.core.schema_fields.object_field import component_field
+from DashAI.back.core.schema_fields.component_field import component_field
 from DashAI.back.core.schema_fields.string_field import string_field
 from DashAI.back.core.schema_fields.utils import fill_objects
