3b28200811f7d9442918d7ed34be97ef03dbd3f6
Lurrea <rodrigo.urrea@ug.uchile.cl>
2024-02-08 09:44:38 -0300

    Change enum values into strings

--- DashAI/back/core/enums/plugin_tags.py
+++ DashAI/back/core/enums/plugin_tags.py
@@ -2,10 +2,10 @@ from enum import Enum
 
 
 class PluginTag(Enum):
-    DashAI = 0
-    Package = 1
-    Task = 2
-    Model = 3
-    Dataloader = 4
-    Converter = 5
-    Explainer = 6
+    DashAI = "DashAI"
+    Package = "Package"
+    Task = "Task"
+    Model = "Model"
+    Dataloader = "Dataloader"
+    Converter = "Converter"
+    Explainer = "Explainer"
