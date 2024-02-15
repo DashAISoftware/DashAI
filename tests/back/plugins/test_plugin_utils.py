d0e1effca3698f5ba8ec8f319e592fce21659c54
Lucas-CE <lucas.carrascoestay@gmail.com>
2024-02-15 15:09:33 -0300

    test: :white_check_mark: Get plugins from pypi test should check dont start dashai names

--- tests/back/plugins/test_plugin_utils.py
+++ tests/back/plugins/test_plugin_utils.py
@@ -64,27 +64,28 @@ def test_get_plugin_data_success():
 def test_get_plugins_from_pypi():
     # Mockear la creación del cliente proxy y su método list_packages
     with patch("xmlrpc.client.ServerProxy") as MockServerProxy:
-        mock_client = Mock()
-        mock_client.list_packages.return_value = [
-            "DashAI",
+        server_proxy_mock = Mock()
+        server_proxy_mock.list_packages.return_value = [
             "image-classification-package",
+            "dashai-tabular-classification-package",
+            "scikit-dashai-learn",
         ]
-        MockServerProxy.return_value = mock_client
+        MockServerProxy.return_value = server_proxy_mock
 
         # Mockear la solicitud HTTP exitosa
-        mock_response = Mock()
+        request_mock = Mock()
         json_return = {
             "info": {
                 "author_email": "dashai <dashaisoftware@gmail.com>",
-                "keywords": "dashai,image classification,package,pytorch,torchvision",
+                "keywords": "dashai,package",
                 "description": "# Description \n",
                 "description_content_type": "text/markdown",
-                "name": "tabular-classification-package",
+                "name": "dashai-tabular-classification-package",
                 "summary": "Tabular Classification Package",
             },
         }
-        mock_response.json.return_value = json_return
-        with patch("requests.get", return_value=mock_response):
+        request_mock.json.return_value = json_return
+        with patch("requests.get", return_value=request_mock):
             plugins = get_plugins_from_pypi()
 
     assert plugins == [
@@ -92,14 +93,11 @@ def test_get_plugins_from_pypi():
             "author_email": "dashai <dashaisoftware@gmail.com>",
             "tags": [
                 {"name": "dashai"},
-                {"name": "image classification"},
                 {"name": "package"},
-                {"name": "pytorch"},
-                {"name": "torchvision"},
             ],
             "description": "# Description \n",
             "description_content_type": "text/markdown",
-            "name": "tabular-classification-package",
+            "name": "dashai-tabular-classification-package",
             "summary": "Tabular Classification Package",
         }
     ]
