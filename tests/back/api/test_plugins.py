375950c87843fc69f9b0eaf20e008a4f11ea73c3
Lucas-CE <lucas.carrascoestay@gmail.com>
2024-02-16 13:00:49 -0300

    test: :white_check_mark: Add mocks to plugin refresh test

--- tests/back/api/test_plugins.py
+++ tests/back/api/test_plugins.py
@@ -1,3 +1,5 @@
+from unittest.mock import Mock, patch
+
 from fastapi.testclient import TestClient
 
 
@@ -20,9 +22,35 @@ def test_post_plugin(client: TestClient):
 
 
 def test_refresh_plugins(client: TestClient):
-    response = client.post("/api/v1/plugin/refresh")
-    assert response.status_code == 201, response.text
-    assert len(response.json()) == 1
+    # Mock to server_proxy
+    server_proxy_mock = Mock()
+    server_proxy_mock.list_packages.return_value = [
+        "image-classification-package",
+        "dashai-tabular-classification-package",
+        "scikit-dashai-learn",
+    ]
+
+    # Mock to request.get
+    request_mock = Mock()
+    json_return = {
+        "info": {
+            "author": "DashAI team",
+            "keywords": "DashAI,Package,Model,Dataloader",
+            "description": "# Description \n",
+            "description_content_type": "text/markdown",
+            "name": "dashai-tabular-classification-package",
+            "summary": "Tabular Classification Package",
+        },
+    }
+    request_mock.json.return_value = json_return
+
+    with patch("xmlrpc.client.ServerProxy") as MockServerProxy:
+        MockServerProxy.return_value = server_proxy_mock
+        with patch("requests.get", return_value=request_mock):
+            response = client.post("/api/v1/plugin/refresh")
+            assert response.status_code == 201, response.text
+            print(response.json())
+            assert len(response.json()) == 1
 
 
 def test_get_all_plugins(client: TestClient):
@@ -41,6 +69,8 @@ def test_get_all_plugins(client: TestClient):
     assert plugins[1]["author"] == "DashAI team"
     assert plugins[1]["tags"][0]["name"] == "DashAI"
     assert plugins[1]["tags"][1]["name"] == "Package"
+    assert plugins[1]["tags"][2]["name"] == "Model"
+    assert plugins[1]["tags"][3]["name"] == "Dataloader"
     assert plugins[1]["status"] == 0
     assert plugins[1]["summary"] == "Tabular Classification Package"
     assert plugins[1]["description_content_type"] == "text/markdown"
