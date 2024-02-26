b72c1817d31acc99255498108d2374f13358005e
Lurrea <rodrigo.urrea@ug.uchile.cl>
2024-02-26 13:06:31 -0300

    add method to replace the $defs field in the generated json schema

--- DashAI/back/config_object.py
+++ DashAI/back/config_object.py
@@ -1,4 +1,7 @@
-from DashAI.back.core.schema_fields.base_schema import BaseSchema
+from DashAI.back.core.schema_fields.base_schema import (
+    BaseSchema,
+    replace_defs_in_schema,
+)
 from DashAI.back.core.schema_fields.utils import fill_objects
 
 
@@ -16,7 +19,8 @@ class ConfigObject:
         dict
             Dictionary representing the Json Schema of the component.
         """
-        return cls.SCHEMA.model_json_schema()
+        schema = cls.SCHEMA.model_json_schema()
+        return replace_defs_in_schema(schema)
 
     def validate_and_transform(self, raw_schema: dict) -> dict:
         schema_instance = self.SCHEMA.model_validate(raw_schema)
