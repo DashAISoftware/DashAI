9b98f22d07b1a9c32be961af2308ebd9517948c6
Nacho Nunez <ignacio.nunez@imfd.cl>
2024-02-27 12:30:29 -0300

    trying to catch pip install error

--- DashAI/back/api/api_v1/endpoints/plugins.py
+++ DashAI/back/api/api_v1/endpoints/plugins.py
@@ -14,19 +14,25 @@ from DashAI.back.api.api_v1.schemas.plugin_params import (
 )
 from DashAI.back.containers import Container
 from DashAI.back.dependencies.database.models import Plugin, Tag
-from DashAI.back.plugins.utils import get_plugins_from_pypi
+from DashAI.back.dependencies.registry import ComponentRegistry
+from DashAI.back.plugins.utils import (
+    get_plugins_from_pypi,
+    install_plugin_from_pypi,
+    register_new_plugins,
+)
 
+logging.basicConfig(level=logging.DEBUG)
 logger = logging.getLogger(__name__)
-logger.setLevel(logging.INFO)
+
 router = APIRouter()
 
 
 @router.get("/")
 @inject
 async def get_plugins(
-    session_factory: Callable[..., ContextManager[Session]] = Provide[
+    session_factory: Callable[..., ContextManager[Session]] = Depends(Provide[
         Container.db.provided.session
-    ],
+    ]),
 ):
     """Retrieve a list of the stored plugins in the database.
 
@@ -62,9 +68,9 @@ async def get_plugins(
 @inject
 async def get_plugin(
     plugin_id: int,
-    session_factory: Callable[..., ContextManager[Session]] = Provide[
+    session_factory: Callable[..., ContextManager[Session]] = Depends(Provide[
         Container.db.provided.session
-    ],
+    ]),
 ):
     """Retrieve the plugin associated with the provided ID.
 
@@ -103,9 +109,9 @@ async def get_plugin(
 @inject
 def add_plugin_to_db(
     raw_plugin: PluginParams,
-    session_factory: Callable[..., ContextManager[Session]] = Provide[
+    session_factory: Callable[..., ContextManager[Session]] = Depends(Provide[
         Container.db.provided.session
-    ],
+    ]),
 ) -> Plugin:
     """Create a Plugin from a PluginParams instance and store it in the DB.
 
@@ -246,6 +252,9 @@ async def update_plugin(
     session_factory: Callable[..., ContextManager[Session]] = Depends(
         Provide[Container.db.provided.session]
     ),
+    component_registry: ComponentRegistry = Depends(
+        Provide[Container.component_registry]
+    ),
 ):
     """Updates the status of a plugin with the provided ID.
 
@@ -258,12 +267,17 @@ async def update_plugin(
     session_factory : Callable[..., ContextManager[Session]]
         A factory that creates a context manager that handles a SQLAlchemy session.
         The generated session can be used to access and query the database.
+    component_registry : ComponentRegistry
+        The current app component registry provided by dependency injection.
 
     Returns
     -------
     Plugin
         The updated plugin.
     """
+    install_plugin_from_pypi("tabular_classification_packagesdads")
+    register_new_plugins(component_registry)
+
     # with session_factory() as db:
     #     try:
     #         plugin = db.get(Plugin, plugin_id)
@@ -282,7 +296,3 @@ async def update_plugin(
     #             status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
     #             detail="Internal database error",
     #         ) from e
-
-    install_plugin_by_pip()
-    update_registry()
-
--- DashAI/back/plugins/utils.py
+++ DashAI/back/plugins/utils.py
@@ -1,4 +1,11 @@
+import subprocess
+import sys
 from typing import List
+
+if sys.version_info < (3, 10):
+    from importlib_metadata import entry_points
+else:
+    from importlib.metadata import entry_points
 
 
 def _get_all_plugins() -> List[str]:
@@ -42,3 +49,42 @@ def _get_plugin_data(plugin_name: str) -> dict:
 def get_plugins_from_pypi() -> List[dict]:
     plugins_names = filter(lambda name: "dashai" in name, _get_all_plugins())
     return [_get_plugin_data(plugin_name) for plugin_name in plugins_names]
+
+
+def available_plugins() -> List[type]:
+
+    # Retrieve plugins groups (DashAI components)
+    plugins = entry_points(group="dashai.plugins")
+
+    # Look for installed plugins
+    plugins_list = []
+    for plugin in plugins:
+        # Retrieve plugin class
+        plugin_class = plugin.load()
+        plugins_list.append(plugin_class)
+
+    return plugins_list
+
+
+def register_new_plugins(component_registry) -> List[type]:
+    installed_plugins = []
+    new_plugins = []
+    for component in component_registry.get_components_by_types():
+        installed_plugins.append(component["class"])
+    for plugin in available_plugins():
+        if plugin not in installed_plugins:
+            print(plugin)
+            new_plugins.append(plugin)
+            if plugin.__name__ != "TabularClassificationModel":
+                component_registry.register_component(plugin)
+    return new_plugins
+
+
+def install_plugin_from_pypi(pypi_plugin_name):
+
+    try:
+        subprocess.run(["pip", "install", pypi_plugin_name],
+                       capture_output=True, text=True, check=True)
+    except subprocess.CalledProcessError as e:
+        raise RuntimeError(
+            f"Couldn't install {pypi_plugin_name}. Error: {e}")
