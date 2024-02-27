c2ba0196eedea243ed6a4e47a686a899f103b2d8
Lurrea <rodrigo.urrea@ug.uchile.cl>
2024-02-27 18:30:01 -0300

    handle boundary cases and add some logs

--- DashAI/back/api/api_v1/endpoints/plugins.py
+++ DashAI/back/api/api_v1/endpoints/plugins.py
@@ -17,7 +17,7 @@ from DashAI.back.dependencies.database.models import Plugin, Tag
 from DashAI.back.plugins.utils import get_plugins_from_pypi
 
 logger = logging.getLogger(__name__)
-logger.setLevel(logging.INFO)
+logger.setLevel(logging.DEBUG)
 router = APIRouter()
 
 
@@ -119,33 +119,70 @@ def add_plugin_to_db(
     List[Plugin]
         A list with the created plugins.
     """
+    logger.debug(
+        "Trying to store plugin metadata in database, plugin name: %s", raw_plugin.name
+    )
     with session_factory() as db:
-        logging.debug("Storing plugin metadata in database.")
-        raw_tags = raw_plugin.tags
         try:
-            plugin = Plugin(
-                name=raw_plugin.name,
-                author=raw_plugin.author,
-                summary=raw_plugin.summary,
-                description=raw_plugin.description,
-                description_content_type=raw_plugin.description_content_type,
-            )
-            db.add(plugin)
+            existing_plugins = db.scalars(
+                db.query(Plugin).filter(Plugin.name == raw_plugin.name)
+            ).all()
+            if existing_plugins != []:
+                logger.debug("Plugin already exists, updating it.")
+                plugin = existing_plugins[0]
+                setattr(plugin, "author", raw_plugin.author)
+                setattr(plugin, "summary", raw_plugin.summary)
+                setattr(plugin, "description", raw_plugin.description)
+                setattr(
+                    plugin,
+                    "description_content_type",
+                    raw_plugin.description_content_type,
+                )
+            else:
+                logger.debug("Storing plugin.")
+                plugin = Plugin(
+                    name=raw_plugin.name,
+                    author=raw_plugin.author,
+                    summary=raw_plugin.summary,
+                    description=raw_plugin.description,
+                    description_content_type=raw_plugin.description_content_type,
+                )
+                db.add(plugin)
+            db.flush()
+
+            for raw_tag in raw_plugin.tags:
+                logger.debug(
+                    (
+                        "Trying to store tag metadata in database, "
+                        "plugin name: %s, tag name: %s"
+                    ),
+                    raw_plugin.name,
+                    raw_tag.name,
+                )
+                existing_tags = db.scalars(
+                    db.query(Tag).filter(
+                        Tag.name == raw_tag.name, Tag.plugin_id == plugin.id
+                    )
+                ).all()
+                if existing_tags == []:
+                    logger.debug("storing tag.")
+                    tag = Tag(
+                        plugin_id=plugin.id,
+                        name=raw_tag.name,
+                    )
+                    db.add(tag)
+                else:
+                    logger.debug(
+                        "Tag %s already exists for plugin %s, aborting",
+                        raw_tag.name,
+                        plugin.name,
+                    )
             db.commit()
             db.refresh(plugin)
-
-            for raw_tag in raw_tags:
-                tag = Tag(
-                    plugin_id=plugin.id,
-                    name=raw_tag.name,
-                )
-                db.add(tag)
-                db.commit()
-            db.refresh(plugin)
-
             return plugin
 
         except exc.SQLAlchemyError as e:
+            db.rollback()
             logger.exception(e)
             raise HTTPException(
                 status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
