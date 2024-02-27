c7d8a717370993c36ba89a63f59b99e24037489f
Cristian Tamblay <cristian.tamblay@ing.uchile.cl>
2024-02-27 17:35:57 -0300

    Testing without components test

--- DashAI/back/dependencies/database/sqlite_database.py
+++ DashAI/back/dependencies/database/sqlite_database.py
@@ -13,6 +13,9 @@ class Base(DeclarativeBase):
     pass
 
 
+n = 0
+
+
 class SQLiteDatabase:
     def __init__(self, db_path: pathlib.Path) -> None:
         _db_path = str(db_path)
@@ -22,7 +25,7 @@ class SQLiteDatabase:
 
         logger.info("Using %s as SQLite path.", db_url)
 
-        self._engine = create_engine(db_url, echo=True)
+        self._engine = create_engine(db_url, echo=False)
         self._session_factory = orm.scoped_session(
             orm.sessionmaker(
                 autocommit=False,
@@ -37,17 +40,22 @@ class SQLiteDatabase:
     @contextmanager
     def session(self) -> Callable[..., ContextManager[Session]]:
         session: Session = self._session_factory()
-
+        global n
         try:
             logger.debug("Generating a new database session.")
+            n = n + 1
+            print("Generating a new database session.", n)
             yield session
         except Exception:
             logger.exception(
                 "Session rollback: An exception was raised while trying to operate "
                 "the database."
             )
+            print("Exception Raised. Rolling back the session.")
             session.rollback()
             raise
         finally:
             logger.debug("Closing the current session.")
+            n = n - 1
+            print("Closing the current session.", n)
             session.close()
