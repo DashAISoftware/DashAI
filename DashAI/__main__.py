7b83c1ba501fd8cb7636af2eb7ca2a54bf735928
Pablo Badilla <pbadilla.torrealba@gmail.com>
2024-03-05 20:49:05 -0300

    Fix logger enum use

--- DashAI/__main__.py
+++ DashAI/__main__.py
@@ -17,6 +17,8 @@ from DashAI.back.app import create_app
 
 
 class LoggingLevel(str, Enum):
+    """Enumeration for logging levels."""
+
     NOTSET = "NOTSET"
     DEBUG = "DEBUG"
     INFO = "INFO"
@@ -25,24 +27,44 @@ class LoggingLevel(str, Enum):
     CRITICAL = "CRITICAL"
 
 
-def open_browser():
+def open_browser() -> None:
     url = "http://localhost:8000/app/"
-    webbrowser.open(url, new=0, autoraise=True)
+    webbrowser.open(url=url, new=0, autoraise=True)
 
 
 def main(
     local_path: Annotated[
         pathlib.Path, typer.Option(help="Path where DashAI files will be stored.")
-    ] = "~/.DashAI",
+    ] = "~/.DashAI",  # type: ignore
     logging_level: Annotated[
         LoggingLevel, typer.Option(help="Logging level.")
     ] = LoggingLevel.INFO,
-):
-    timer = threading.Timer(1, open_browser)
+) -> None:
+    """Main function for DashAI package.
+
+    This function is executed when the package is called from the command line.
+    It starts a timer to open the browser and runs the Dash application using Uvicorn.
+
+
+    Parameters
+    ----------
+    local_path : pathlib.Path,
+        Path where DashAI local files will be stored, by default "~/.DashAI".
+    logging_level : LoggingLevel
+        Logging level. Defaults to LoggingLevel.INFO.
+
+    """
+    logging.getLogger(name=__package__).setLevel(level=logging_level.value)
+    logger = logging.getLogger(__name__)
+
+    logger.info("Starting DashAI application.")
+    logger.info("Opening browser.")
+    timer = threading.Timer(interval=1, function=open_browser)
     timer.start()
 
+    logger.info("Starting Uvicorn server application.")
     uvicorn.run(
-        create_app(local_path=local_path, logging_level=logging_level),
+        app=create_app(local_path=local_path, logging_level=logging_level.value),
         host="127.0.0.1",
         port=8000,
     )
