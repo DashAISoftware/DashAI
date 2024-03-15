5b1287e41ccd72c83282a30e60049de38aae2eee
Lurrea <rodrigo.urrea@ug.uchile.cl>
2024-03-15 10:07:49 -0300

    new procces for jobs

--- DashAI/back/dependencies/job_queues/job_queue.py
+++ DashAI/back/dependencies/job_queues/job_queue.py
@@ -1,7 +1,8 @@
+import asyncio
 import logging
+from multiprocessing import Process
 
 from dependency_injector.wiring import Provide, inject
-from sqlalchemy import exc
 
 from DashAI.back.containers import Container
 from DashAI.back.dependencies.job_queues import BaseJobQueue
@@ -13,7 +14,9 @@ logger = logging.getLogger(__name__)
 
 @inject
 async def job_queue_loop(
-    stop_when_queue_empties: bool,
+    component_registry=Provide[Container.component_registry],
+    session_factory=Provide[Container.db.provided.session],
+    config=Provide[Container.config],
     job_queue: BaseJobQueue = Provide[Container.job_queue],
 ):
     """Loop function to execute all the pending jobs in the job queue.
@@ -28,11 +31,26 @@ async def job_queue_loop(
         boolean to set the while loop condition.
 
     """
-    while not job_queue.is_empty() if stop_when_queue_empties else True:
-        try:
+    try:
+        while True:
             job: BaseJob = await job_queue.async_get()
-            job.run()
-        except exc.SQLAlchemyError as e:
-            logger.exception(e)
-        except JobError as e:
-            logger.exception(e)
+            job_process = Process(
+                target=job.run,
+                daemon=True,
+            )
+            job_process.start()
+
+            while job_process.is_alive():
+                logger.debug(f"Awaiting {job.id} process for 1 second.")
+                await asyncio.sleep(1)
+
+            job_process.join()
+
+    except JobError as e:
+        logger.exception(e)
+        raise RuntimeError(
+            f"Error in the training execution loop: {e}.\nShutting down the app."
+        ) from e
+    except KeyboardInterrupt:
+        logger.info("Shutting down the app")
+        return
