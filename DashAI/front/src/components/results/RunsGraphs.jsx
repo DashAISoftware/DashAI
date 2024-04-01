48f670b2867c59daf3f6bae3b7e93ad4a5772af5
danielroco <daniel.roco.vera@gmail.com>
2024-04-01 16:43:29 -0300

    Only plot experiments with data

--- DashAI/front/src/components/results/RunsGraphs.jsx
+++ DashAI/front/src/components/results/RunsGraphs.jsx
@@ -39,6 +39,14 @@ function RunsGraphs( {experimentId} ) {
       const experiment = await getExperimentById(parseInt(experimentId));
       return { runs, experiment };
     } catch (error) {
+      enqueueSnackbar(`Error while trying to obtain data of the experiment id: ${experimentId}`);
+      if (error.response) {
+        console.error("Response error:", error.message);
+      } else if (error.request) {
+        console.error("Request error", error.request);
+      } else {
+        console.error("Unknown Error", error.message);
+      }
     }
   };
 
@@ -66,67 +74,69 @@ function RunsGraphs( {experimentId} ) {
           return metrics;
         });
       
-      // Order in which metrics appear
-      const metricsOrder = Object.keys(newFilteredDataWithMetrics[0]);
-      const metricsValuesOrder = Object.keys(newFilteredDataWithMetrics[0][metricsOrder[0]]);
-      const concatenatedMetrics = metricsOrder.map(metricType => metricType.split('_')[0]).concat(metricsValuesOrder);
-      setConcatenatedMetrics(concatenatedMetrics);
+      if (newFilteredDataWithMetrics.length > 0){
+        // Order in which metrics appear
+        const metricsOrder = Object.keys(newFilteredDataWithMetrics[0]);
+        const metricsValuesOrder = Object.keys(newFilteredDataWithMetrics[0][metricsOrder[0]]);
+        const concatenatedMetrics = metricsOrder.map(metricType => metricType.split('_')[0]).concat(metricsValuesOrder);
+        setConcatenatedMetrics(concatenatedMetrics);
 
-      const tabularMetrics = [];
-      metricsOrder.forEach(metricType => {
-        metricsValuesOrder.forEach(metric => {
-          tabularMetrics.push(`${metricType.split('_')[0]} ${metric}`);
-        });
-      });
-  
-      if (showCustomMetrics){
-        parameterIndex = selectedParameters.map(param => tabularMetrics.indexOf(param));
-      }else if (!showCustomMetrics && selectedGeneralMetric.length > 0){
-        setTabularMetrics(tabularMetrics);
-        const criteria = {};
-        concatenatedMetrics.forEach(item => {
-          criteria[item] = item;
-        });
-
-        tabularMetrics.forEach((metric, index) => {
-          Object.entries(criteria).forEach(([metricName, substring]) => {
-            if (selectedGeneralMetric === metricName && metric.includes(substring)) {
-              parameterIndex.push(index);
-              generalParameters.push(metric);
-            }
+        const tabularMetrics = [];
+        metricsOrder.forEach(metricType => {
+          metricsValuesOrder.forEach(metric => {
+            tabularMetrics.push(`${metricType.split('_')[0]} ${metric}`);
           });
         });
-      } else {
-        parameterIndex = [];
-      }
 
-      filteredData.forEach(item => {
-        const numericValues = [];
-
-        metricsOrder.forEach(metricType => {
-          if (metricType.endsWith('metrics')) {
-            const metrics = item[metricType];
-            metricsValuesOrder.forEach(metric => {
-              numericValues.push(metrics[metric]);
+        if (showCustomMetrics){
+          parameterIndex = selectedParameters.map(param => tabularMetrics.indexOf(param));
+        }else if (!showCustomMetrics && selectedGeneralMetric.length > 0){
+          setTabularMetrics(tabularMetrics);
+          const criteria = {};
+          concatenatedMetrics.forEach(item => {
+            criteria[item] = item;
+          });
+  
+          tabularMetrics.forEach((metric, index) => {
+            Object.entries(criteria).forEach(([metricName, substring]) => {
+              if (selectedGeneralMetric === metricName && metric.includes(substring)) {
+                parameterIndex.push(index);
+                generalParameters.push(metric);
+              }
             });
-          }
+          });
+        } else {
+          parameterIndex = [];
+        }
+  
+        filteredData.forEach(item => {
+          const numericValues = [];
+  
+          metricsOrder.forEach(metricType => {
+            if (metricType.endsWith('metrics')) {
+              const metrics = item[metricType];
+              metricsValuesOrder.forEach(metric => {
+                numericValues.push(metrics[metric]);
+              });
+            }
+          });
+  
+          const relevantNumericValues = parameterIndex.map(index => numericValues[index]);
+          graphsMaking(graphsToView, item, relevantNumericValues, showCustomMetrics, selectedParameters, generalParameters, pieCounter);
+          pieCounter+= 1;
         });
-
-        const relevantNumericValues = parameterIndex.map(index => numericValues[index]);
-        graphsMaking(graphsToView, item, relevantNumericValues, showCustomMetrics, selectedParameters, generalParameters, pieCounter);
-        pieCounter+= 1;
-      });
-
-      // Call the Layouts to use
-      let {generalLayout, pieLayout} = layoutMaking(selectedChart, graphsToView)
-
-      // Call the Graphs to use, make sure to see the correct order in RunsGraphsLayout.jsx
-      const graphsToViewKeys = Object.keys(graphsToView);
-      let radarValues = graphsToView[graphsToViewKeys[0]];
-      let barValues = graphsToView[graphsToViewKeys[1]];
-      let pieValues = graphsToView[graphsToViewKeys[2]];
-
-      setChartData({ generalLayout, pieLayout, radarValues, barValues, pieValues });
+  
+        // Call the Layouts to use
+        let {generalLayout, pieLayout} = layoutMaking(selectedChart, graphsToView)
+  
+        // Call the Graphs to use, make sure to see the correct order in RunsGraphsLayout.jsx
+        const graphsToViewKeys = Object.keys(graphsToView);
+        let radarValues = graphsToView[graphsToViewKeys[0]];
+        let barValues = graphsToView[graphsToViewKeys[1]];
+        let pieValues = graphsToView[graphsToViewKeys[2]];
+  
+        setChartData({ generalLayout, pieLayout, radarValues, barValues, pieValues });
+      }
     };
     processData();
   }, [selectedParameters, selectedChart]);
