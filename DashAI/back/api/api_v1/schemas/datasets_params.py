05bcc27fad0479d28e4c4a8f66bb9733bed516f2
Cristian Tamblay <cristian.tamblay@ing.uchile.cl>
2024-05-07 13:02:29 -0400

    Modified datasets endpoint and schema for new implementation

--- DashAI/back/api/api_v1/schemas/datasets_params.py
+++ DashAI/back/api/api_v1/schemas/datasets_params.py
@@ -3,26 +3,26 @@ from typing import Dict, Union
 from pydantic import BaseModel
 
 
-class DataLoaderParams(BaseModel):
-    separator: Union[str, None] = ","
-    data_key: Union[str, None] = "data"
-
-
 class SplitParams(BaseModel):
     train_size: float = 0.8
     test_size: float = 0.1
     val_size: float = 0.1
-    seed: int = None
+
+
+class MoreOptionsParams(BaseModel):
     shuffle: bool = True
-    stratify: bool = False
+    seed: int = 0
+    statify: bool = False
 
 
 class DatasetParams(BaseModel):
     dataloader: str
-    dataset_name: str
+    name: str
     splits_in_folders: bool = False
     splits: SplitParams
-    dataloader_params: DataLoaderParams
+    more_options: MoreOptionsParams
+    separator: Union[str, None] = ","
+    data_key: Union[str, None] = "data"
 
 
 class ColumnSpecItemParams(BaseModel):
