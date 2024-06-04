6628dc546be1040a0480cbb3b4b251d34b85801d
Javiera Labrin <javiera.labrin@ug.uchile.cl>
2024-06-03 23:15:21 -0400

    Add linear_regression.py model

---
+++ DashAI/back/models/scikit_learn/linear_regression.py
@@ -0,0 +1,8 @@
+from sklearn.linear_model import LinearRegression as _LinearRegression
+
+from DashAI.back.models.regression_model import RegressionModel
+from DashAI.back.models.scikit_learn.sklearn_like_model import SklearnLikeModel
+
+
+class RidgeRegression(RegressionModel, SklearnLikeModel, _LinearRegression):
+    """Scikit-learn's Linear Regression wrapper for DashAI."""
