fda0a26e8e95b97bd6ba0edef3bb2d6c76139062
Javiera Labrin <javiera.labrin@ug.uchile.cl>
2024-06-03 22:38:30 -0400

    Add SVMRegression class for Support Vector Regression in linearSVR.py

---
+++ DashAI/back/models/scikit_learn/linearSVR.py
@@ -0,0 +1,9 @@
+from sklearn.svm import LinearSVR as _LinearSVR
+
+from DashAI.back.models.regression_model import RegressionModel
+from DashAI.back.models.scikit_learn.sklearn_like_model import SklearnLikeModel
+
+
+class SVMRegression(RegressionModel, SklearnLikeModel, _LinearSVR):
+    """Scikit-learn's Linear Support Vector Regression (LinearSVR)
+    wrapper for DashAI."""
