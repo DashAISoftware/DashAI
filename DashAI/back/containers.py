2fb01dd1d8f3f45eed3d0964059084eedac8181a
Nacho Nunez <ignacio.nunez@imfd.cl>
2024-06-15 23:22:40 -0400

    Unused EmptyContainer class removed

--- DashAI/back/containers.py
+++ DashAI/back/containers.py
@@ -22,32 +22,3 @@ class Container(containers.DeclarativeContainer):
     component_registry = providers.Singleton(
         ComponentRegistry, initial_components=config.INITIAL_COMPONENTS
     )
-
-
-class EmptyContainer(containers.DeclarativeContainer):
-    wiring_config = containers.WiringConfiguration(
-        packages=["DashAI", "tests"],
-        auto_wire=True,
-    )
-
-    config = providers.Configuration()
-
-    db = providers.Singleton(
-        provides=SQLiteDatabase,
-        db_path=config.SQLITE_DB_PATH,
-        logging_level=config.LOGGING_LEVEL,
-    )
-    job_queue = providers.Singleton(SimpleJobQueue)
-    component_registry = providers.Singleton(
-        ComponentRegistry,
-        initial_components=[],
-    )
-
-
-def get_container(container_type: str = "local"):
-    if container_type == "local":
-        return Container()
-    elif container_type == "empty":
-        return EmptyContainer()
-    else:
-        raise ValueError("Unknown container type")
