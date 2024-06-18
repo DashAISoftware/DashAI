7553e6d69f46a906585ff15c9cf45fe4c82e641b
Javiera Labrin <javiera.labrin@ug.uchile.cl>
2024-06-18 11:28:47 -0400

    imported RMSE and MAE metrics from regression to containers.py

--- DashAI/back/containers.py
+++ DashAI/back/containers.py
@@ -5,7 +5,7 @@ from DashAI.back.dependencies.database import SQLiteDatabase
 from DashAI.back.dependencies.job_queues import SimpleJobQueue
 from DashAI.back.dependencies.registry import ComponentRegistry
 from DashAI.back.job.model_job import ModelJob
-from DashAI.back.metrics import F1, Accuracy, Bleu, Precision, Recall
+from DashAI.back.metrics import F1, MAE, RMSE, Accuracy, Bleu, Precision, Recall
 from DashAI.back.models import (
     SVC,
     DecisionTreeClassifier,
@@ -81,6 +81,8 @@ class Container(containers.DeclarativeContainer):
             Precision,
             Recall,
             Bleu,
+            MAE,
+            RMSE,
             # Jobs
             ModelJob,
         ],
