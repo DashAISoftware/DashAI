0dc681e99530915a5a9bb7e111c670e492c485e8
Nacho Nunez <ignacio.nunez@imfd.cl>
2024-07-04 16:26:47 -0400

    mlp model added to models init

--- DashAI/back/dataloaders/classes/image_dataloader.py
+++ DashAI/back/dataloaders/classes/image_dataloader.py
@@ -6,13 +6,46 @@ from beartype import beartype
 from datasets import DatasetDict, load_dataset
 from starlette.datastructures import UploadFile
 
-from DashAI.back.dataloaders.classes.dataloader import BaseDataLoader
+from DashAI.back.core.schema_fields import (
+    bool_field,
+    none_type,
+    schema_field,
+    string_field,
+)
+from DashAI.back.core.schema_fields.base_schema import BaseSchema
+from DashAI.back.dataloaders.classes.dataloader import (
+    BaseDataLoader,
+    DataloaderMoreOptionsSchema,
+    DatasetSplitsSchema,
+)
+
+
+class ImageDataloaderSchema(BaseSchema):
+    name: schema_field(
+        none_type(string_field()),
+        "",
+        (
+            "Custom name to register your dataset. If no name is specified, "
+            "the name of the uploaded file will be used."
+        ),
+    )  # type: ignore
+    splits_in_folders: schema_field(
+        bool_field(),
+        False,
+        (
+            "If your data has folders that define the splits select 'true', "
+            "otherwise 'false'."
+        ),
+    )  # type: ignore
+    splits: DatasetSplitsSchema
+    more_options: DataloaderMoreOptionsSchema
 
 
 class ImageDataLoader(BaseDataLoader):
     """Data loader for data from image files."""
 
     COMPATIBLE_COMPONENTS = ["ImageClassificationTask"]
+    SCHEMA = ImageDataloaderSchema
 
     @beartype
     def load_data(
@@ -40,7 +73,7 @@ class ImageDataLoader(BaseDataLoader):
             A HuggingFace's Dataset with the loaded data.
         """
         if isinstance(filepath_or_buffer, str):
-            dataset = load_dataset("imagefolder", data_files=filepath_or_buffer)
+            dataset = load_dataset("imagefolder", data_dir=filepath_or_buffer)
         elif isinstance(filepath_or_buffer, UploadFile):
             if filepath_or_buffer.content_type == "application/zip":
                 extracted_files_path = self.extract_files(temp_path, filepath_or_buffer)
