948c05224a286e096fdb44edca6d42d61d2a2efc
Lucas-CE <lucas.carrascoestay@gmail.com>
2024-07-25 21:45:13 -0400

    fix: list_field should be imported in init

--- DashAI/back/core/schema_fields/__init__.py
+++ DashAI/back/core/schema_fields/__init__.py
@@ -5,10 +5,11 @@ from DashAI.back.core.schema_fields.component_field import component_field
 from DashAI.back.core.schema_fields.enum_field import enum_field
 from DashAI.back.core.schema_fields.float_field import float_field
 from DashAI.back.core.schema_fields.int_field import int_field
+from DashAI.back.core.schema_fields.list_field import list_field
 from DashAI.back.core.schema_fields.none_type import none_type
+from DashAI.back.core.schema_fields.optimizer_float_field import optimizer_float_field
+from DashAI.back.core.schema_fields.optimizer_int_field import optimizer_int_field
 from DashAI.back.core.schema_fields.schema_field import schema_field
 from DashAI.back.core.schema_fields.string_field import string_field
 from DashAI.back.core.schema_fields.union_type import union_type
 from DashAI.back.core.schema_fields.utils import fill_objects
-from DashAI.back.core.schema_fields.optimizer_float_field import optimizer_float_field
-from DashAI.back.core.schema_fields.optimizer_int_field import optimizer_int_field
