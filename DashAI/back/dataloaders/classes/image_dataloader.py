b067fad8b093eaf9d92929826dfa0d754976ed75
Lucas-CE <lucas.carrascoestay@gmail.com>
2024-07-25 23:17:09 -0400

    fix: Fix support for uploading zip files in ImageDataLoader

--- DashAI/back/dataloaders/classes/image_dataloader.py
+++ DashAI/back/dataloaders/classes/image_dataloader.py
@@ -4,7 +4,7 @@ from typing import Any, Dict, Union
 
 from beartype import beartype
 from datasets import DatasetDict, load_dataset
-from starlette.datastructures import UploadFile
+from starlette.datastructures import Headers, UploadFile
 
 from DashAI.back.core.schema_fields import none_type, schema_field, string_field
 from DashAI.back.core.schema_fields.base_schema import BaseSchema
@@ -62,6 +62,12 @@ class ImageDataLoader(BaseDataLoader):
         if isinstance(filepath_or_buffer, str):
             dataset = load_dataset("imagefolder", data_files=filepath_or_buffer)
         elif isinstance(filepath_or_buffer, UploadFile):
+            if filepath_or_buffer.content_type == "application/x-zip-compressed":
+                filepath_or_buffer = UploadFile(
+                    filename=filepath_or_buffer.filename,
+                    file=filepath_or_buffer.file,
+                    headers=Headers({"Content-Type": "application/zip"}),
+                )
             if filepath_or_buffer.content_type == "application/zip":
                 extracted_files_path = self.extract_files(temp_path, filepath_or_buffer)
                 dataset = load_dataset(
