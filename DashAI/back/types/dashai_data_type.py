1e1a18d41fa1c89633d2bc580ec9a54bf01faae5
Vicente Olivares <vicente.olivares@ug.uchile.cl>
2024-07-30 23:57:08 -0400

    Delete from_hf_feature method in DashAIDataType

--- DashAI/back/types/dashai_data_type.py
+++ DashAI/back/types/dashai_data_type.py
@@ -1,13 +1,2 @@
-from abc import ABC, abstractmethod
-from typing import Union
-
-from datasets import ClassLabel, Image, Value
-
-feature_type = Union[Value, Image, ClassLabel]
-
-
-class DashAIDataType(ABC):
-    @abstractmethod
-    @staticmethod
-    def from_hf_feature(hf_feature: feature_type):
-        raise NotImplementedError
+class DashAIDataType:
+    pass
