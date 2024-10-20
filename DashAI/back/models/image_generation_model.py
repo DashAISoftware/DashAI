969cbab48bcc7104bb0e6601217b87f228328311
Irozuku <emiliodu@gmail.com>
2024-10-20 18:45:49 -0300

    feat: Add new image generation model

---
+++ DashAI/back/models/image_generation_model.py
@@ -0,0 +1,7 @@
+from DashAI.back.models.base_model import BaseModel
+
+
+class ImageGenerationModel(BaseModel):
+    """Class for models associated to ImageGenerationProcess."""
+
+    COMPATIBLE_COMPONENTS = ["ImageGenerationProcess"]
